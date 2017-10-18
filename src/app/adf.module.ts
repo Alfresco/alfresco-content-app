a391ffc33d6af1d8d7d20fc177d7863e837ae91a
Denys Vuika <denis.vuyka@gmail.com>
2017-10-18 18:21:58 +0100

    integrate ADF 1.9.0 libs

---
+++ src/app/adf.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+// ADF modules
+import { CoreModule } from 'ng2-alfresco-core';
+import { DataTableModule } from 'ng2-alfresco-datatable';
+import { ViewerModule } from 'ng2-alfresco-viewer';
+import { DocumentListModule } from 'ng2-alfresco-documentlist';
+import { LoginModule } from 'ng2-alfresco-login';
+import { UploadModule } from 'ng2-alfresco-upload';
+import { SearchModule } from 'ng2-alfresco-search';
+
+export function modules() {
+  return [
+    // ADF modules
+    CoreModule,
+    DataTableModule,
+    DocumentListModule,
+    LoginModule,
+    SearchModule,
+    UploadModule,
+    ViewerModule
+  ];
+}
+
+@NgModule({
+  imports: modules(),
+  exports: modules()
+})
+export class AdfModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AdfModule } from './adf.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdfModule
   ],
   providers: [],
   bootstrap: [AppComponent]
