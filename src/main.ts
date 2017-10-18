9c382ff0acf4b740417646b647a693daa299ca42
Denys Vuika <denis.vuyka@gmail.com>
2017-10-18 17:48:22 +0100

    configure extra dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'hammerjs';
+
+import pdfjsLib from 'pdfjs-dist';
+pdfjsLib.PDFJS.workerSrc = 'pdf.worker.js';
+
 if (environment.production) {
   enableProdMode();
 }
