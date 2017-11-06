d10255b8438238a12f21566e0ceab9943faa933b
Denys Vuika <denis.vuyka@gmail.com>
2017-11-06 14:11:04 +0000

    production build setup

---
+++ postbuild-dev.js
@@ -0,0 +1,26 @@
+/*!
+ * @license
+ * Copyright 2017 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var fs = require('fs');
+var config = require('./dist/app.config.json');
+
+config.ecmHost = 'http://localhost:8080';
+
+fs.writeFileSync(
+    './dist/app.config.json',
+    JSON.stringify(config, null, 4)
+);
