50705a6c6d265b9fc3081f7e9a0c423d2eefd966
suzana_dirla <suzana@dirla.ro>
2018-03-01 16:58:55 +0200

    [ACA-1162] Destination picker has incorrect title when opened for multiple selection of items

--- src/app/common/services/node-actions.service.spec.ts
+++ src/app/common/services/node-actions.service.spec.ts
@@ -354,7 +354,7 @@ describe('NodeActionsService', () => {
             expect(testContentNodeSelectorComponentData.data.rowFilter({node: destinationFolder})).toBeDefined();
             expect(testContentNodeSelectorComponentData.data.imageResolver({node: destinationFolder})).toBeDefined();
             expect(testContentNodeSelectorComponentData.data.title).toBe('NODE_SELECTOR.COPY_ITEMS');
-            expect(translationService.instant).toHaveBeenCalledWith('NODE_SELECTOR.COPY_ITEMS', {name: ''});
+            expect(translationService.instant).toHaveBeenCalledWith('NODE_SELECTOR.COPY_ITEMS', {name: '', number: 2});
 
             destinationFolder.entry['allowableOperations'] = ['update'];
             expect(testContentNodeSelectorComponentData.data.imageResolver({node: destinationFolder})).toBeDefined();
@@ -377,7 +377,7 @@ describe('NodeActionsService', () => {
             expect(spyOnBatchOperation).toHaveBeenCalled();
             expect(testContentNodeSelectorComponentData).toBeDefined();
             expect(testContentNodeSelectorComponentData.data.title).toBe('NODE_SELECTOR.COPY_ITEM');
-            expect(translationService.instant).toHaveBeenCalledWith('NODE_SELECTOR.COPY_ITEM', {name: 'entry-name'});
+            expect(translationService.instant).toHaveBeenCalledWith('NODE_SELECTOR.COPY_ITEM', {name: 'entry-name', number: 1});
         });
 
         it('should use the ContentNodeSelectorComponentData object without file name in title, if no name exists', () => {
@@ -397,7 +397,7 @@ describe('NodeActionsService', () => {
             expect(spyOnBatchOperation).toHaveBeenCalled();
             expect(testContentNodeSelectorComponentData).toBeDefined();
             expect(testContentNodeSelectorComponentData.data.title).toBe('NODE_SELECTOR.COPY_ITEMS');
-            expect(translationService.instant).toHaveBeenCalledWith('NODE_SELECTOR.COPY_ITEMS', {name: ''});
+            expect(translationService.instant).toHaveBeenCalledWith('NODE_SELECTOR.COPY_ITEMS', {name: '', number: 1});
         });
 
     });
