06e4cb0a56e3a3f061c052f17a8cc37081132d15
Denys Vuika <denis.vuyka@gmail.com>
2018-05-01 20:38:12 +0100

    fix search pagination

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -68,7 +68,7 @@ export class SearchComponent implements OnInit {
         this.data = nodePaging;
     }
 
-    onRefreshPagination(pagination: Pagination) {
+    onPaginationChanged(pagination: Pagination) {
         this.maxItems = pagination.maxItems;
         this.skipCount = pagination.skipCount;
 
