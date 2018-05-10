c5cbd5bf2f7debdc57edd2a647afae818324b8e4
Denys Vuika <denis.vuyka@gmail.com>
2018-05-10 17:03:07 +0100

    upgrade to adf app config pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,7 +58,6 @@ import { NodePermanentDeleteDirective } from './common/directives/node-permanent
 import { NodeUnshareDirective } from './common/directives/node-unshare.directive';
 import { NodeInfoDirective } from './common/directives/node-info.directive';
 import { NodeVersionsDirective } from './common/directives/node-versions.directive';
-import { AppConfigPipe } from './common/pipes/app-config.pipe';
 import { VersionManagerDialogAdapterComponent } from './components/versions-dialog/version-manager-dialog-adapter.component';
 import { BrowsingFilesService } from './common/services/browsing-files.service';
 import { ContentManagementService } from './common/services/content-management.service';
@@ -111,7 +110,6 @@ import { SearchComponent } from './components/search/search.component';
         NodeUnshareDirective,
         NodeInfoDirective,
         NodeVersionsDirective,
-        AppConfigPipe,
         VersionManagerDialogAdapterComponent,
         SearchComponent
     ],
