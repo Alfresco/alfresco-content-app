b6938060aecec7859fa42a518808afc1f93d69b9
Denys Vuika <denys.vuika@gmail.com>
2018-06-12 19:16:01 +0100

    fix snackbar selector

--- e2e/pages/page.ts
+++ e2e/pages/page.ts
@@ -35,7 +35,7 @@ export abstract class Page {
         overlay: by.css('.cdk-overlay-container'),
         dialogContainer: by.css('.mat-dialog-container'),
         snackBarContainer: '.cdk-overlay-pane snack-bar-container.mat-snack-bar-container',
-        snackBar: 'simple-snack-bar',
+        snackBar: 'mat-simple-snackbar',
         snackBarAction: 'button.mat-simple-snackbar-action'
     };
 
