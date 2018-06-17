2c6136ef59201111697f1de7e1111859fd46e8d8
Denys Vuika <denys.vuika@gmail.com>
2018-06-17 13:18:38 +0100

    remove old workarounds (#427)
    * remove ADF 2.3 workarounds
    
    * remove old workarounds for styles
    
    * remove old document list workarounds
    
    * style fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,10 +25,7 @@
 
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import {
-    PageTitleService, UserPreferencesService, AppConfigService,
-    FileModel, UploadService
-} from '@alfresco/adf-core';
+import { PageTitleService, AppConfigService, FileModel, UploadService } from '@alfresco/adf-core';
 import { ElectronService } from '@ngstack/electron';
 import { Store } from '@ngrx/store';
 import { AppStore } from './store/states/app.state';
@@ -44,14 +41,10 @@ export class AppComponent implements OnInit {
         private route: ActivatedRoute,
         private router: Router,
         private pageTitle: PageTitleService,
-        preferences: UserPreferencesService,
         private store: Store<AppStore>,
         private config: AppConfigService,
         private electronService: ElectronService,
         private uploadService: UploadService) {
-        // TODO: remove once ADF 2.3.0 is out (needs bug fixes)
-        preferences.defaults.supportedPageSizes = config.get('pagination.supportedPageSizes');
-        preferences.defaults.paginationSize = config.get('pagination.size');
     }
 
     ngOnInit() {
