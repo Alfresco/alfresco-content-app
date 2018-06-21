da5ac36a7e13092f174933204f4bb1de91495da7
Denys Vuika <denys.vuika@gmail.com>
2018-06-21 07:31:16 +0100

    library effects and actions

--- src/app/store/effects.ts
+++ src/app/store/effects.ts
@@ -29,3 +29,4 @@ export * from './effects/router.effects';
 export * from './effects/snackbar.effects';
 export * from './effects/viewer.effects';
 export * from './effects/search.effects';
+export * from './effects/library.effects';
