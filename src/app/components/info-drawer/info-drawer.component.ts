19ffa4c4bbf5c93ed72f25d13096c05bff7f8a5d
Cilibiu Bogdan <pionnegru@users.noreply.github.com>
2018-07-02 21:33:09 +0300

    [ACA-1513] Metadata - image EXIF option not rendering (#477)
    * get aspectNames for image type if there is none
    
    * checkspell and checktype
    
    * cspell

--- src/app/components/info-drawer/info-drawer.component.ts
+++ src/app/components/info-drawer/info-drawer.component.ts
@@ -74,9 +74,22 @@ export class InfoDrawerComponent implements OnChanges {
                 // workaround for Favorite files
                 this.loadNodeInfo(entry.id);
             } else {
-                this.displayNode = this.node.entry;
+                // workaround Recent
+                if (this.isTypeImage(entry) && !this.hasAspectNames(entry)) {
+                    this.loadNodeInfo(this.node.entry.id);
+                } else {
+                    this.displayNode = this.node.entry;
+                }
             }
         }
+    }
+
+    private hasAspectNames(entry: MinimalNodeEntryEntity) {
+        return entry.aspectNames && entry.aspectNames.includes('exif:exif');
+    }
+
+    private isTypeImage(entry) {
+        return entry.content.mimeType.includes('image/');
     }
 
     private loadNodeInfo(nodeId: string) {
