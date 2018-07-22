98906942dc10cd03b2a4aede13dbbb99b9cee73f
Denys Vuika <denys.vuika@gmail.com>
2018-07-22 12:52:28 +0100

    chaining rules, "not" evaluator (#527)
    * chaining rules, "not" evaluator
    
    * improved evaluators
    
    * fix code

--- src/app/extensions/evaluators/app.evaluators.ts
+++ src/app/extensions/evaluators/app.evaluators.ts
@@ -26,6 +26,15 @@
 import { Node } from 'alfresco-js-api';
 import { RuleContext, RuleParameter } from '../rule.extensions';
 
+export function isTrashcan(context: RuleContext, ...args: RuleParameter[]): boolean {
+    const { url } = context.navigation;
+    return url && url.startsWith('/trashcan');
+}
+
+export function isNotTrashcan(context: RuleContext, ...args: RuleParameter[]): boolean {
+    return !isTrashcan(context, ...args);
+}
+
 export function canCreateFolder(context: RuleContext, ...args: RuleParameter[]): boolean {
     const folder = context.navigation.currentFolder;
     if (folder) {
@@ -37,7 +46,7 @@ export function canCreateFolder(context: RuleContext, ...args: RuleParameter[]):
 export function canDownloadSelection(context: RuleContext, ...args: RuleParameter[]): boolean {
     if (!context.selection.isEmpty) {
         return context.selection.nodes.every(node => {
-            return node.entry && (node.entry.isFile || node.entry.isFolder);
+            return node.entry && (node.entry.isFile || node.entry.isFolder || !!node.entry.nodeId);
         });
     }
     return false;
