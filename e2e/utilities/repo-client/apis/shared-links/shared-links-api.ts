cafb50ec3233e9fb580930226e638fb271f6aa9e
Adina Parpalita <Adina.Parpalita@ness.com>
2018-08-13 10:50:08 +0300

    use alfresco-js-api-node in e2e trashcanApi

--- e2e/utilities/repo-client/apis/shared-links/shared-links-api.ts
+++ e2e/utilities/repo-client/apis/shared-links/shared-links-api.ts
@@ -71,6 +71,6 @@ export class SharedLinksApi extends RepoApiNew {
             }
         };
 
-        return Utils.retryCall(sharedFiles);
+        return await Utils.retryCall(sharedFiles);
     }
 }
