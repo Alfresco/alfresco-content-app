b9b7859fe5bd58cfddd50985e235b027609abb6d
Adina Parpalita <Adina.Parpalita@ness.com>
2018-09-07 14:17:54 +0300

    add specific methods for single click on item name link automate tests for Single click on item name

--- protractor.conf.js
+++ protractor.conf.js
@@ -74,23 +74,5 @@ exports.config = {
             useFullTestName: false,
             reportFailedUrl: true
         }));
-
-        return browser.driver.executeScript(disableCSSAnimation);
-
-        function disableCSSAnimation() {
-            var css = '* {' +
-                '-webkit-transition-duration: 0s !important;' +
-                'transition-duration: 0s !important;' +
-                '-webkit-animation-duration: 0s !important;' +
-                'animation-duration: 0s !important;' +
-                '}',
-            head = document.head || document.getElementsByTagName('head')[0],
-            style = document.createElement('style');
-
-            style.type = 'text/css';
-            style.appendChild(document.createTextNode(css));
-            head.appendChild(style);
-        }
-
     }
 };
