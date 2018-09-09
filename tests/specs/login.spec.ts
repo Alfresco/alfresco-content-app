f13e0b7763d6ff1fe0398f0d566a99913c30b82f
Denys Vuika <denys.vuika@gmail.com>
2018-09-09 12:18:01 +0100

    more tests

--- tests/specs/login.spec.ts
+++ tests/specs/login.spec.ts
@@ -8,7 +8,21 @@ describe('Login', () => {
 
     beforeAll(async () => {
         page = await browser.newPage();
+    });
+
+    beforeEach(async () => {
         await page.goto(settings.appUrl);
+    });
+
+
+    afterEach(async () => {
+        await page.evaluate(() => {
+            localStorage.clear();
+        });
+    });
+
+    afterAll(async () => {
+        await page.close();
     });
 
     it('should have correct title', async () => {
@@ -27,6 +41,38 @@ describe('Login', () => {
             '.adf-layout-header',
             el => (el ? true : false)
         );
+
         expect(navbar).toBe(true);
+    });
+
+    describe('with invalid credentials', () => {
+        it('disabled submit button when password is empty - [C280072]', async () => {
+            await page.type('#username', 'any-username');
+
+            const disabled = await page.evaluate(() => {
+                const attr = document.getElementById('login-button').getAttribute('disabled');
+                return attr === '' || attr === 'true';
+            });
+
+            if (!disabled) {
+                await page.screenshot({ path: 'tests/screenshots/C280072.png' });
+                fail('submit button is enabled');
+            }
+        });
+
+        it('disabled submit button when username is empty - [C280070]', async () => {
+            await page.$eval('#username', (el: any) => el.value = '');
+            await page.type('#password', 'any-password');
+
+            const disabled = await page.evaluate(() => {
+                const attr = document.getElementById('login-button').getAttribute('disabled');
+                return attr === '' || attr === 'true';
+            });
+
+            if (!disabled) {
+                await page.screenshot({ path: 'tests/screenshots/C280070.png' });
+                fail('submit button is enabled');
+            }
+        });
     });
 });
