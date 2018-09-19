5075d74ef31c800e593488a579514a3d105544d2
Denys Vuika <denys.vuika@gmail.com>
2018-09-19 19:46:54 +0100

    deployment test

--- e2e/suites/application/page-titles.test.ts
+++ e2e/suites/application/page-titles.test.ts
@@ -31,7 +31,7 @@ import { RepoClient } from '../../utilities/repo-client/repo-client';
 import { Utils } from '../../utilities/utils';
 
 
-describe('Page titles', () => {
+fdescribe('Page titles', () => {
     const loginPage = new LoginPage();
     const logoutPage = new LogoutPage();
     const page = new BrowsingPage();
