6e201f03b5444e810c6f57fac03162f7ef4860a0
Denys Vuika <denys.vuika@gmail.com>
2018-09-19 13:33:13 +0100

    remove obsolete projects

--- projects/my-extension/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of my-extension
- */
-
-export * from './lib/my-extension.service';
-export * from './lib/my-extension.component';
-export * from './lib/my-extension.module';
