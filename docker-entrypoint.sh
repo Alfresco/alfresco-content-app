a120a50ada832861e58457aa8878fb9660b4b1c9
svidrascu <sergiu.vidrascu@ness.com>
2018-09-21 18:03:18 +0300

    fix #653 : add basepath modification at runtime trough env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 if [[ $ACSURL ]]; then
   sed -i s%{protocol}//{hostname}{:port}%"$ACSURL"%g /usr/share/nginx/html/app.config.json
 fi
+if [[ $BASEPATH ]]; then
+  sed -i s%href=\"./"%href=\""$BASEPATH"\"%g /usr/share/nginx/html/index.html
+fi
 
 nginx -g "daemon off;"
