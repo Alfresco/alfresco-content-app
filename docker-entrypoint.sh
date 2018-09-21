776b2e9e1845b65d06921b598f0a4e17d1f81a9c
Sergiu Vidrascu <vsergiu@hotmail.com>
2018-09-21 20:08:38 +0300

    fix #653 - fixing issue caused by missing \ in docker entrypoint (#655)
    * fix #653 : add basepath modification at runtime trough env vars
    
    * Fix typo in sed command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ if [[ $ACSURL ]]; then
   sed -i s%{protocol}//{hostname}{:port}%"$ACSURL"%g /usr/share/nginx/html/app.config.json
 fi
 if [[ $BASEPATH ]]; then
-  sed -i s%href=\"./"%href=\""$BASEPATH"\"%g /usr/share/nginx/html/index.html
+  sed -i s%href=\"./\"%href=\""$BASEPATH"\"%g /usr/share/nginx/html/index.html
 fi
 
 nginx -g "daemon off;"
