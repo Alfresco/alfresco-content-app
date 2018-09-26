23df2ad6a2cf4cca1e27abc1b51b9d392baa6dc3
Adina Parpalita <adina.parpalita@ness.com>
2018-09-26 12:09:16 +0300

    [ACA-1760] use async / await in list-views suite (#669)

--- e2e/suites/list-views/shared-files.test.ts
+++ e2e/suites/list-views/shared-files.test.ts
@@ -29,125 +29,126 @@ import { Utils } from '../../utilities/utils';
 import { RepoClient } from '../../utilities/repo-client/repo-client';
 
 describe('Shared Files', () => {
-    const username = `user-${Utils.random()}`;
-    const password = username;
+  const username = `user-${Utils.random()}`;
+  const password = username;
 
-    const siteName = `site-${Utils.random()}`;
-    const fileAdmin = `file-${Utils.random()}.txt`;
+  const siteName = `site-${Utils.random()}`;
+  const fileAdmin = `file-${Utils.random()}.txt`;
 
-    const folderUser = `folder-${Utils.random()}`;
-    const file1User = `file1-${Utils.random()}.txt`; let file1Id;
-    const file2User = `file2-${Utils.random()}.txt`; let file2Id;
-    const file3User = `file3-${Utils.random()}.txt`; let file3Id;
-    const file4User = `file4-${Utils.random()}.txt`; let file4Id;
+  const folderUser = `folder-${Utils.random()}`;
+  const file1User = `file1-${Utils.random()}.txt`; let file1Id;
+  const file2User = `file2-${Utils.random()}.txt`; let file2Id;
+  const file3User = `file3-${Utils.random()}.txt`; let file3Id;
+  const file4User = `file4-${Utils.random()}.txt`; let file4Id;
 
-    const apis = {
-        admin: new RepoClient(),
-        user: new RepoClient(username, password)
-    };
+  const apis = {
+    admin: new RepoClient(),
+    user: new RepoClient(username, password)
+  };
 
-    const loginPage = new LoginPage();
-    const logoutPage = new LogoutPage();
-    const sharedFilesPage = new BrowsingPage();
-    const { dataTable, breadcrumb } = sharedFilesPage;
+  const loginPage = new LoginPage();
+  const logoutPage = new LogoutPage();
+  const sharedFilesPage = new BrowsingPage();
+  const { dataTable, breadcrumb } = sharedFilesPage;
 
-    beforeAll(done => {
-        apis.admin.people.createUser({ username })
-            .then(() => apis.admin.sites.createSite(siteName, SITE_VISIBILITY.PUBLIC))
-            .then(() => apis.admin.sites.addSiteMember(siteName, username, SITE_ROLES.SITE_CONSUMER))
-            .then(() => apis.admin.nodes.createFiles([ fileAdmin ], `Sites/${siteName}/documentLibrary`))
-            .then(resp => apis.admin.shared.shareFileById(resp.entry.id))
+  beforeAll(async (done) => {
+    await apis.admin.people.createUser({ username });
+    await apis.admin.sites.createSite(siteName, SITE_VISIBILITY.PUBLIC);
+    await apis.admin.sites.addSiteMember(siteName, username, SITE_ROLES.SITE_CONSUMER);
+    const docLibId = await apis.admin.sites.getDocLibId(siteName);
+    const nodeId = (await apis.admin.nodes.createFile(fileAdmin, docLibId)).entry.id;
+    await apis.admin.shared.shareFileById(nodeId);
 
-            .then(() => apis.user.nodes.createFolders([ folderUser ]))
-            .then(() => apis.user.nodes.createFiles([ file1User ], folderUser)).then(resp => file1Id = resp.entry.id)
-            .then(() => apis.user.nodes.createFile(file2User)).then(resp => file2Id = resp.entry.id)
-            .then(() => apis.user.nodes.createFile(file3User)).then(resp => file3Id = resp.entry.id)
-            .then(() => apis.user.nodes.createFile(file4User)).then(resp => file4Id = resp.entry.id)
-            .then(() => apis.user.shared.shareFilesByIds([file1Id, file2Id, file3Id, file4Id]))
+    const folderId = (await apis.user.nodes.createFolder(folderUser)).entry.id;
+    file1Id = (await apis.user.nodes.createFile(file1User, folderId)).entry.id;
+    file2Id = (await apis.user.nodes.createFile(file2User)).entry.id;
+    file3Id = (await apis.user.nodes.createFile(file3User)).entry.id;
+    file4Id = (await apis.user.nodes.createFile(file4User)).entry.id;
+    await apis.user.shared.shareFilesByIds([file1Id, file2Id, file3Id, file4Id]);
 
-            .then(() => apis.user.shared.waitForApi({ expect: 5 }))
-            .then(() => apis.user.nodes.deleteNodeById(file2Id))
-            .then(() => apis.user.shared.unshareFile(file3User))
+    await apis.user.shared.waitForApi({ expect: 5 });
+    await apis.user.nodes.deleteNodeById(file2Id);
+    await apis.user.shared.unshareFile(file3User);
 
-            .then(() => loginPage.loginWith(username))
-            .then(done);
+    await loginPage.loginWith(username);
+    done();
+  });
+
+  beforeEach(async (done) => {
+    await sharedFilesPage.sidenav.navigateToLinkByLabel(SIDEBAR_LABELS.SHARED_FILES);
+    await dataTable.waitForHeader();
+    done();
+  });
+
+  afterEach(async (done) => {
+    await sharedFilesPage.refresh();
+    done();
+  });
+
+  afterAll(async (done) => {
+    await Promise.all([
+      apis.admin.sites.deleteSite(siteName),
+      apis.user.nodes.deleteNodes([ folderUser ]),
+      logoutPage.load()
+    ]);
+    done();
+  });
+
+  it('has the correct columns - [C213113]', async () => {
+    const labels = [ 'Name', 'Location', 'Size', 'Modified', 'Modified by', 'Shared by' ];
+    const elements = labels.map(label => dataTable.getColumnHeaderByLabel(label));
+
+    expect(await dataTable.getColumnHeaders().count()).toBe(6 + 1, 'Incorrect number of columns');
+
+    await elements.forEach(async (element, index) => {
+      expect(await element.isPresent()).toBe(true, `"${labels[index]}" is missing`);
     });
+  });
 
-    beforeEach(done => {
-        sharedFilesPage.sidenav.navigateToLinkByLabel(SIDEBAR_LABELS.SHARED_FILES)
-            .then(() => dataTable.waitForHeader())
-            .then(done);
-    });
+  it('default sorting column - [C213115]', async () => {
+    expect(await dataTable.getSortedColumnHeader().getText()).toBe('Modified');
+    expect(await dataTable.getSortingOrder()).toBe('desc');
+  });
 
-    afterEach(done => {
-        sharedFilesPage.refresh().then(done);
-    });
+  it('displays the files shared by everyone - [C213114]', async () => {
+    expect(await dataTable.getRowByName(fileAdmin).isPresent()).toBe(true, `${fileAdmin} not displayed`);
+    expect(await dataTable.getRowByName(file1User).isPresent()).toBe(true, `${file1User} not displayed`);
+  });
 
-    afterAll(done => {
-        Promise.all([
-            apis.admin.sites.deleteSite(siteName),
-            apis.user.nodes.deleteNodes([ folderUser ]),
-            logoutPage.load()
-        ])
-        .then(done);
-    });
+  it(`file not displayed if it's been deleted - [C213117]`, async () => {
+    expect(await dataTable.getRowByName(file2User).isPresent()).toBe(false, `${file2User} is displayed`);
+  });
 
-    it('has the correct columns - [C213113]', () => {
-        const labels = [ 'Name', 'Location', 'Size', 'Modified', 'Modified by', 'Shared by' ];
-        const elements = labels.map(label => dataTable.getColumnHeaderByLabel(label));
+  // TODO: disabled cause the api is slow to update. Find a way to wait until the file is unshared
+  xit('unshared file is not displayed - [C213118]', async (done) => {
+    await apis.user.shared.waitForApi({ expect: 4 });
+    expect(await dataTable.getRowByName(file3User).isPresent()).toBe(false, `${file3User} is displayed`);
+    done();
+  });
 
-        expect(dataTable.getColumnHeaders().count()).toBe(6 + 1, 'Incorrect number of columns');
+  it('Location column displays the parent folder of the file - [C213665]', async () => {
+    expect(await dataTable.getItemLocationTileAttr(file4User)).toEqual('Personal Files');
+    expect(await dataTable.getItemLocation(fileAdmin).getText()).toEqual(siteName);
+    expect(await dataTable.getItemLocation(file1User).getText()).toEqual(folderUser);
+  });
 
-        elements.forEach((element, index) => {
-            expect(element.isPresent()).toBe(true, `"${labels[index]}" is missing`);
-        });
-    });
+  it('Location column redirect - file in user Home - [C213666]', async () => {
+    await dataTable.clickItemLocation(file4User);
+    expect(await breadcrumb.getAllItems()).toEqual([ 'Personal Files' ]);
+  });
 
-    it('default sorting column - [C213115]', () => {
-        expect(dataTable.getSortedColumnHeader().getText()).toBe('Modified');
-        expect(dataTable.getSortingOrder()).toBe('desc');
-    });
+  it('Location column redirect - file in folder - [C280490]', async () => {
+    await dataTable.clickItemLocation(file1User);
+    expect(await breadcrumb.getAllItems()).toEqual([ 'Personal Files', folderUser ]);
+  });
 
-    it('displays the files shared by everyone - [C213114]', () => {
-        expect(dataTable.getRowByName(fileAdmin).isPresent()).toBe(true, `${fileAdmin} not displayed`);
-        expect(dataTable.getRowByName(file1User).isPresent()).toBe(true, `${file1User} not displayed`);
-    });
+  it('Location column redirect - file in site - [C280491]', async () => {
+    await dataTable.clickItemLocation(fileAdmin);
+    expect(await breadcrumb.getAllItems()).toEqual([ 'File Libraries', siteName ]);
+  });
 
-    it(`file not displayed if it's been deleted - [C213117]`, () => {
-        expect(dataTable.getRowByName(file2User).isPresent()).toBe(false, `${file2User} is displayed`);
-    });
-
-    // TODO: disabled cause the api is slow to update. Find a way to wait until the file is unshared
-    xit('unshared file is not displayed - [C213118]', async () => {
-        apis.user.shared.waitForApi({ expect: 4 })
-            .then(() => {
-                expect(dataTable.getRowByName(file3User).isPresent()).toBe(false, `${file3User} is displayed`);
-            });
-    });
-
-    it('Location column displays the parent folder of the file - [C213665]', () => {
-        expect(dataTable.getItemLocationTileAttr(file4User)).toEqual('Personal Files');
-        expect(dataTable.getItemLocation(fileAdmin).getText()).toEqual(siteName);
-        expect(dataTable.getItemLocation(file1User).getText()).toEqual(folderUser);
-    });
-
-    it('Location column redirect - file in user Home - [C213666]', () => {
-        dataTable.clickItemLocation(file4User)
-            .then(() => expect(breadcrumb.getAllItems()).toEqual([ 'Personal Files' ]));
-    });
-
-    it('Location column redirect - file in folder - [C280490]', () => {
-        dataTable.clickItemLocation(file1User)
-            .then(() => expect(breadcrumb.getAllItems()).toEqual([ 'Personal Files', folderUser ]));
-    });
-
-    it('Location column redirect - file in site - [C280491]', () => {
-        dataTable.clickItemLocation(fileAdmin)
-            .then(() => expect(breadcrumb.getAllItems()).toEqual([ 'File Libraries', siteName ]));
-    });
-
-    it('Location column displays a tooltip with the entire path of the file - [C213667]', () => {
-        expect(dataTable.getItemLocationTileAttr(fileAdmin)).toEqual(`File Libraries/${siteName}`);
-        expect(dataTable.getItemLocationTileAttr(file1User)).toEqual(`Personal Files/${folderUser}`);
-    });
+  it('Location column displays a tooltip with the entire path of the file - [C213667]', async () => {
+    expect(await dataTable.getItemLocationTileAttr(fileAdmin)).toEqual(`File Libraries/${siteName}`);
+    expect(await dataTable.getItemLocationTileAttr(file1User)).toEqual(`Personal Files/${folderUser}`);
+  });
 });
