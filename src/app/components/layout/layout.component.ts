48a16e0ba9d8aef221cfaa5e93265e853837294f
Bogdan Cilibiu <cilibiu.bogdan@yahoo.com>
2018-10-17 20:41:53 +0300

    check sidenav visibility condition on reload

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -74,6 +74,10 @@ export class LayoutComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
+    this.hideSidenav = this.hideConditions.some(el =>
+      this.router.routerState.snapshot.url.includes(el)
+    );
+
     if (!this.minimizeSidenav) {
       this.expandedSidenav = this.getSidenavState();
     } else {
