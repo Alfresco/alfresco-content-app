40ee1fc2e663044f3618090189ed61c015a3acdb
Adina Parpalita <Adina.Parpalita@ness.com>
2019-01-14 11:14:09 +0200

    more fixes

--- e2e/suites/viewer/viewer-general.test.ts
+++ e2e/suites/viewer/viewer-general.test.ts
@@ -23,7 +23,6 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { protractor, browser } from 'protractor';
 import { LoginPage, BrowsingPage } from '../../pages/pages';
 import { FILES, SITE_VISIBILITY } from '../../configs';
 import { RepoClient } from '../../utilities/repo-client/repo-client';
@@ -83,7 +82,7 @@ describe('Viewer general', () => {
     });
 
     afterEach(async (done) => {
-        await browser.actions().sendKeys(protractor.Key.ESCAPE).perform();
+        await Utils.pressEscape();
         done();
     });
 
@@ -139,7 +138,7 @@ describe('Viewer general', () => {
     });
 
     it('Viewer opens for a file from File Libraries - [C284633]', async () => {
-      await page.clickFileLibrariesAndWait();
+      await page.goToMyLibrariesAndWait();
         await dataTable.doubleClickOnRowByName(siteUser);
         await dataTable.waitForHeader();
         await dataTable.doubleClickOnRowByName(fileInSite);
