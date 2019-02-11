75ad83d27252ac4a0b5d07e35e639b4868cba99d
Adina Parpalita <adina.parpalita@ness.com>
2019-02-11 10:24:11 +0200

    move comment as well

--- e2e/suites/actions/special-permissions-available-actions.test.ts
+++ e2e/suites/actions/special-permissions-available-actions.test.ts
@@ -392,11 +392,11 @@ describe('Granular permissions available actions : ', () => {
         expect(await toolbar.isViewPresent()).toBe(false, `View is not displayed for ${folder1}`);
         expect(await toolbar.isDownloadPresent()).toBe(true, `Download is not displayed for ${folder1}`);
         expect(await toolbar.isViewDetailsPresent()).toBe(true, `View details is not displayed for ${folder1}`);
-        // TODO: change expect to false when ACA-1737 is done
         expect(await toolbar.isSharePresent()).toBe(false, `Share is displayed`);
 
         await toolbar.openMoreMenu();
 
+        // TODO: change expect to false when ACA-1737 is done
         expect(await toolbar.menu.isEditFolderPresent()).toBe(true, `Edit folder is displayed for ${folder1}`);
         expect(await toolbar.menu.isEditOfflinePresent()).toBe(false, `Edit offline is displayed for ${folder1}`);
         expect(await toolbar.menu.isCancelEditingPresent()).toBe(false, `Cancel editing is displayed for ${folder1}`);
