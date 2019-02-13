69102c82119b5e885a33ed552dd948bd56f5b8de
Denys Vuika <denys.vuika@gmail.com>
2019-02-13 20:49:48 +0000

    update unit tests

--- src/app/directives/library-favorite.directive.spec.ts
+++ src/app/directives/library-favorite.directive.spec.ts
@@ -56,7 +56,7 @@ describe('LibraryFavoriteDirective', () => {
   let selection;
 
   setupTestBed({
-    imports: [CoreModule],
+    imports: [CoreModule.forRoot()],
     declarations: [TestComponent, LibraryFavoriteDirective],
     providers: [
       {
