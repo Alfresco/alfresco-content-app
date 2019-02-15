010b545ed4a5f4bde975fd09982f29b77fc18261
Cilibiu Bogdan <bogdan.cilibiu@ness.com>
2019-02-15 09:21:36 +0200

    restore version over delete node

---
+++ src/app/components/upload-dialog/file-uploading-dialog.component.ts
@@ -0,0 +1,167 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  FileModel,
+  FileUploadCompleteEvent,
+  FileUploadDeleteEvent,
+  FileUploadErrorEvent,
+  FileUploadStatus,
+  UploadService
+} from '@alfresco/adf-core';
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import { Subscription, merge } from 'rxjs';
+import { FileUploadingListComponent } from './file-uploading-list.component';
+
+@Component({
+  selector: 'app-file-uploading-dialog',
+  templateUrl: './file-uploading-dialog.component.html',
+  styleUrls: ['./file-uploading-dialog.component.scss']
+})
+export class FileUploadingDialogComponent implements OnInit, OnDestroy {
+  @ViewChild('uploadList')
+  uploadList: FileUploadingListComponent;
+
+  /** Dialog position. Can be 'left' or 'right'. */
+  @Input()
+  position = 'right';
+
+  /** Emitted when a file in the list has an error. */
+  @Output()
+  error: EventEmitter<any> = new EventEmitter();
+
+  filesUploadingList: FileModel[] = [];
+  isDialogActive = false;
+  totalCompleted = 0;
+  totalErrors = 0;
+  isDialogMinimized = false;
+  isConfirmation = false;
+
+  private listSubscription: Subscription;
+  private counterSubscription: Subscription;
+  private fileUploadSubscription: Subscription;
+  private errorSubscription: Subscription;
+
+  constructor(
+    private uploadService: UploadService,
+    private changeDetector: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.listSubscription = this.uploadService.queueChanged.subscribe(
+      (fileList: FileModel[]) => {
+        this.filesUploadingList = fileList;
+
+        if (this.filesUploadingList.length) {
+          this.isDialogActive = true;
+        }
+      }
+    );
+
+    this.counterSubscription = merge(
+      this.uploadService.fileUploadComplete,
+      this.uploadService.fileUploadDeleted
+    ).subscribe((event: FileUploadDeleteEvent | FileUploadCompleteEvent) => {
+      this.totalCompleted = event.totalComplete;
+      this.changeDetector.detectChanges();
+    });
+
+    this.errorSubscription = this.uploadService.fileUploadError.subscribe(
+      (event: FileUploadErrorEvent) => {
+        this.totalErrors = event.totalError;
+        this.changeDetector.detectChanges();
+      }
+    );
+
+    this.fileUploadSubscription = this.uploadService.fileUpload.subscribe(
+      () => {
+        this.changeDetector.detectChanges();
+      }
+    );
+
+    this.uploadService.fileDeleted.subscribe(objId => {
+      if (this.filesUploadingList) {
+        const file = this.filesUploadingList.find(item => {
+          return item.data.entry.id === objId;
+        });
+        if (file) {
+          file.status = FileUploadStatus.Cancelled;
+          this.changeDetector.detectChanges();
+        }
+      }
+    });
+  }
+
+  /**
+   * Toggle confirmation message.
+   */
+  toggleConfirmation() {
+    this.isConfirmation = !this.isConfirmation;
+
+    if (this.isDialogMinimized) {
+      this.isDialogMinimized = false;
+    }
+  }
+
+  /**
+   * Cancel uploads and hide confirmation
+   */
+  cancelAllUploads() {
+    this.toggleConfirmation();
+
+    this.uploadList.cancelAllFiles();
+  }
+
+  /**
+   * Toggle dialog minimized state.
+   */
+  toggleMinimized(): void {
+    this.isDialogMinimized = !this.isDialogMinimized;
+    this.changeDetector.detectChanges();
+  }
+
+  /**
+   * Dismiss dialog
+   */
+  close(): void {
+    this.isConfirmation = false;
+    this.totalCompleted = 0;
+    this.totalErrors = 0;
+    this.filesUploadingList = [];
+    this.isDialogActive = false;
+    this.isDialogMinimized = false;
+    this.uploadService.clearQueue();
+    this.changeDetector.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.uploadService.clearQueue();
+    this.listSubscription.unsubscribe();
+    this.counterSubscription.unsubscribe();
+    this.fileUploadSubscription.unsubscribe();
+    this.errorSubscription.unsubscribe();
+  }
+}
---
+++ src/app/components/upload-dialog/upload.module.ts
@@ -0,0 +1,66 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * @license
+ * Alfresco Example Content Application
+ *
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoreModule } from '@alfresco/adf-core';
+import { FileUploadingDialogComponent } from './file-uploading-dialog.component';
+import { FileUploadingListRowComponent } from './file-uploading-list-row.component';
+import { FileUploadingListComponent } from './file-uploading-list.component';
+import { FileUploadErrorPipe } from './file-upload-error.pipe';
+
+@NgModule({
+  imports: [CommonModule, CoreModule.forChild()],
+  declarations: [
+    FileUploadingDialogComponent,
+    FileUploadingListRowComponent,
+    FileUploadingListComponent,
+    FileUploadErrorPipe
+  ],
+  exports: [
+    FileUploadingDialogComponent,
+    FileUploadingListRowComponent,
+    FileUploadingListComponent,
+    FileUploadErrorPipe
+  ]
+})
+export class AppUploadingDialogModule {}
