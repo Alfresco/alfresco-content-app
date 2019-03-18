98ba2e7954e5c9e2257b94b88d865091b279a617
Adina Parpalita <Adina.Parpalita@ness.com>
2019-03-18 09:57:51 +0200

    update copyright year

--- e2e/suites/actions/context-menu-multiple-selection.test.ts
+++ e2e/suites/actions/context-menu-multiple-selection.test.ts
@@ -2,7 +2,7 @@
  * @license
  * Alfresco Example Content Application
  *
- * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * Copyright (C) 2005 - 2019 Alfresco Software Limited
  *
  * This file is part of the Alfresco Example Content Application.
  * If the software was purchased under a paid Alfresco license, the terms of
