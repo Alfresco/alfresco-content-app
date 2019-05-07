ce2bfd9ce26fad2ecc7a26daef6e6d621f3a2882
Cilibiu Bogdan <bogdan.cilibiu@ness.com>
2019-05-07 16:36:35 +0300

    set CONTEXT_MENU_DIRECTION  value

--- src/app/components/context-menu/context-menu.service.ts
+++ src/app/components/context-menu/context-menu.service.ts
@@ -1,15 +1,55 @@
-import { Injectable, Injector, ComponentRef, ElementRef } from '@angular/core';
+/*!
+ * @license
+ * Alfresco Example Content Application
+ *
+ * Copyright (C) 2005 - 2019 Alfresco Software Limited
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Injectable, Injector, ComponentRef } from '@angular/core';
 import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
 import { ContextMenuOverlayRef } from './context-menu-overlay';
 import { ContextMenuComponent } from './context-menu.component';
 import { ContextmenuOverlayConfig } from './interfaces';
+import { UserPreferencesService } from '@alfresco/adf-core';
+import { Directionality } from '@angular/cdk/bidi';
+import { CONTEXT_MENU_DIRECTION } from './direction.token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContextMenuService {
-  constructor(private injector: Injector, private overlay: Overlay) {}
+  private direction: Directionality;
+
+  constructor(
+    private injector: Injector,
+    private overlay: Overlay,
+    private userPreferenceService: UserPreferencesService
+  ) {
+    this.userPreferenceService
+      .select('textOrientation')
+      .subscribe(textOrientation => {
+        this.direction = textOrientation;
+      });
+  }
 
   open(config: ContextmenuOverlayConfig) {
     const overlay = this.createOverlay(config);
@@ -49,6 +89,7 @@ export class ContextMenuService {
     const injectionTokens = new WeakMap();
 
     injectionTokens.set(ContextMenuOverlayRef, contextmenuOverlayRef);
+    injectionTokens.set(CONTEXT_MENU_DIRECTION, this.direction);
 
     return new PortalInjector(this.injector, injectionTokens);
   }
