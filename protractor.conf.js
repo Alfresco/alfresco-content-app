1a54e5b62fc8962bba827ec4e55edcead354db6a
Adina Parpalita <Adina.Parpalita@ness.com>
2019-05-09 13:42:48 +0300

    try to increase timeout

--- protractor.conf.js
+++ protractor.conf.js
@@ -94,7 +94,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 60000,
+    defaultTimeoutInterval: 100000,
     print: function() {}
   },
 
