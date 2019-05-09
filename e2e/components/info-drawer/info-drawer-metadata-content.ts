f8d33de6c9adeea2811102e78911fb23c377151c
Adina Parpalita <Adina.Parpalita@ness.com>
2019-05-09 12:20:27 +0300

    add “date item” to spell ignore and remove commented code

--- e2e/components/info-drawer/info-drawer-metadata-content.ts
+++ e2e/components/info-drawer/info-drawer-metadata-content.ts
@@ -31,10 +31,7 @@ export class ContentMetadata extends Component {
   private static selectors = {
     root: 'adf-content-metadata-card',
 
-    // contentMetadataProperties: '.adf-metadata-properties',
     expandedPanel: '.mat-expansion-panel.mat-expanded',
-    // panelTitle: '.mat-expansion-panel-header-title',
-    // panelContent: '.mat-expansion-panel-content',
     propertyList: '.adf-property-list',
     property: '.adf-property',
     propertyLabel: '.adf-property-label',
@@ -48,36 +45,21 @@ export class ContentMetadata extends Component {
   expandedPanel: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.expandedPanel));
   propertyList: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.propertyList));
   propertyListElements: ElementArrayFinder = this.component.all(by.css(ContentMetadata.selectors.property));
-  // propertyLabel: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.propertyLabel));
   propertyValue: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.propertyValue));
   editPropertiesButton: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.editProperties));
   lessInfoButton: ElementFinder = this.component.element(by.cssContainingText(ContentMetadata.selectors.moreLessInformation, 'Less information'));
   moreInfoButton: ElementFinder = this.component.element(by.cssContainingText(ContentMetadata.selectors.moreLessInformation, 'More information'));
 
-  // propertiesPanel: ElementFinder = this.component.element(by.css(`[data-automation-id='adf-metadata-group-properties']`));
   imagePropertiesPanel: ElementFinder = this.component.element(by.css(`[data-automation-id='adf-metadata-group-APP.CONTENT_METADATA.EXIF_GROUP_TITLE']`));
   expandedImagePropertiesPanel: ElementFinder = this.component.element(by.css(`[data-automation-id='adf-metadata-group-APP.CONTENT_METADATA.EXIF_GROUP_TITLE'].mat-expanded`));
-
-  dateTimePropertyEdit: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.editDateItem));
-
-  // editPropertyButton: ElementFinder = this.component.element(by.css(ContentMetadata.selectors.editProperty));
 
   constructor(ancestor?: ElementFinder) {
     super(ContentMetadata.selectors.root, ancestor);
   }
 
-
-  // async isPropertiesListDisplayed() {
-  //   return (await browser.isElementPresent(this.propertyList)) && (await this.propertyList.isDisplayed());
-  // }
-
   async isPropertiesListExpanded() {
     return await browser.isElementPresent(this.expandedPanel);
   }
-
-  // async isPropertyDisplayed(label: string) {
-  //   return await browser.isElementPresent(by.cssContainingText(ContentMetadata.selectors.propertyLabel, label));
-  // }
 
   async waitForImagePropertiesPanelToExpand() {
     return await browser.wait(EC.visibilityOf(this.expandedImagePropertiesPanel), BROWSER_WAIT_TIMEOUT);
@@ -103,22 +85,6 @@ export class ContentMetadata extends Component {
       });
   }
 
-  // async getVisibleEditableProperties() {
-  //   return await this.propertyListElements
-  //     .filter(async (elem) => await elem.isElementPresent(by.css(ContentMetadata.selectors.editProperty)))
-  //     .filter(async (elem) => await elem.isDisplayed())
-  //     .map(async (elem) => await elem.$(ContentMetadata.selectors.propertyLabel).getText());
-  // }
-
-  // async isDateTimePropertyEditable() {
-  //   return await this.dateTimePropertyEdit.isDisplayed();
-  // }
-
-  // async isFlashPropertyEditable() {
-  //   const elemClass = await this.component.element(by.css(`.mat-checkbox[title='Toggle value']`)).getAttribute('class');
-  //   return !elemClass.includes(`.mat-checkbox-disabled`);
-  // }
-
   async isEditPropertiesButtonEnabled() {
     return (await browser.isElementPresent(this.editPropertiesButton)) && (await this.editPropertiesButton.isEnabled());
   }
@@ -126,10 +92,6 @@ export class ContentMetadata extends Component {
   async isLessInfoButtonEnabled() {
     return (await browser.isElementPresent(this.lessInfoButton)) && (await this.lessInfoButton.isEnabled());
   }
-
-  // async isLessInfoButtonPresent() {
-  //   return await browser.isElementPresent(this.lessInfoButton);
-  // }
 
   async isMoreInfoButtonEnabled() {
     return (await browser.isElementPresent(this.moreInfoButton)) && (await this.moreInfoButton.isEnabled());
@@ -142,10 +104,6 @@ export class ContentMetadata extends Component {
   async isMoreInfoButtonDisplayed() {
     return await browser.isElementPresent(this.moreInfoButton);
   }
-
-  // async clickEditPropertiesButton() {
-  //   return await this.editPropertiesButton.click();
-  // }
 
   async clickLessInformationButton() {
     return await this.lessInfoButton.click();
