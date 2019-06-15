5491545ac9eddb8dd795aa65bc328ea14167a378
dhrn <14145706+dhrn@users.noreply.github.com>
2019-06-15 08:47:59 +0530

    [ACA-2471] - close preview action (#1129)

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "/alfresco": {
-        "target": "http://0.0.0.0:8080",
-        "secure": false,
-        "changeOrigin": true,
-        // workaround for REPO-2260
-        onProxyRes: function (proxyRes, req, res) {
-            const header = proxyRes.headers['www-authenticate'];
-            if (header && header.startsWith('Basic')) {
-                proxyRes.headers['www-authenticate'] = 'x' + header;
-            }
-        }
+  '/alfresco': {
+    target: 'http://0.0.0.0:8080',
+    secure: false,
+    changeOrigin: true,
+    // workaround for REPO-2260
+    onProxyRes: function(proxyRes, req, res) {
+      const header = proxyRes.headers['www-authenticate'];
+      if (header && header.startsWith('Basic')) {
+        proxyRes.headers['www-authenticate'] = 'x' + header;
+      }
     }
+  }
 };
