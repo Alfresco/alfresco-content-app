55fe3f8399bfbf906a3400b2486eafb2bddc2dcb
pionnegru <cilibiu.bogdan@yahoo.com>
2019-12-12 10:11:18 +0200

    templates actions

--- projects/aca-shared/store/src/actions/template.actions.ts
+++ projects/aca-shared/store/src/actions/template.actions.ts
@@ -23,28 +23,14 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from './actions/app.actions';
-export * from './actions/library.actions';
-export * from './actions/node.actions';
-export * from './actions/router.actions';
-export * from './actions/search.actions';
-export * from './actions/snackbar.actions';
-export * from './actions/upload.actions';
-export * from './actions/viewer.actions';
-export * from './actions/metadata-aspect.actions';
-export * from './actions/template.actions';
+import { Action } from '@ngrx/store';
 
-export * from './effects/dialog.effects';
-export * from './effects/router.effects';
-export * from './effects/snackbar.effects';
+export enum TemplateActionTypes {
+  CreateFileFromTemplate = 'CREATE_FILE_FROM_TEMPLATE'
+}
 
-export * from './models/delete-status.model';
-export * from './models/deleted-node-info.model';
-export * from './models/node-info.model';
-export * from './models/search-option.model';
+export class CreateFileFromTemplate implements Action {
+  readonly type = TemplateActionTypes.CreateFileFromTemplate;
 
-export * from './selectors/app.selectors';
-
-export * from './states/app.state';
-
-export * from './store.module';
+  constructor() {}
+}
