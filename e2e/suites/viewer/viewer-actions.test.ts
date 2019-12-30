803c4a9734e0d0f289298940c7988474a25b686c
Martin Mueller <damadden88@googlemail.com>
2019-12-30 19:03:40 +0100

    Add e2e test for showing 'Editing Offline' when a new version was uploaded and the node was locked before

--- e2e/suites/viewer/viewer-actions.test.ts
+++ e2e/suites/viewer/viewer-actions.test.ts
@@ -221,6 +221,26 @@ describe('Viewer actions', () => {
       expect(await apis.user.nodes.getFileVersionLabel(filePersonalFilesId)).toEqual('2.0', 'File has incorrect version label');
     });
 
+    it('Upload new version action when node is locked - [MNT-21058]', async () => {
+
+      await dataTable.doubleClickOnRowByName(fileForCancelEditing);
+      await viewer.waitForViewerToOpen();
+
+      await toolbar.openMoreMenu();
+      expect(await toolbar.menu.isCancelEditingActionPresent()).toBe(true, `'Cancel Editing' button should be shown`);
+      expect(await toolbar.menu.isEditOfflineActionPresent()).toBe(false, `'Edit Offline' shouldn't be shown`);
+
+      await toolbar.menu.clickMenuItem('Upload New Version');
+      await Utils.uploadFileNewVersion(docxFile);
+      await page.waitForDialog();
+
+      await uploadNewVersionDialog.clickUpload();
+
+      await toolbar.openMoreMenu();
+      expect(await toolbar.menu.isCancelEditingActionPresent()).toBe(false, `'Cancel Editing' button shouldn't be shown`);
+      expect(await toolbar.menu.isEditOfflineActionPresent()).toBe(true, `'Edit Offline' should be shown`);
+    });
+
     it('Full screen action - [C279282]', async () => {
       await dataTable.doubleClickOnRowByName(docxPersonalFiles);
       await viewer.waitForViewerToOpen();
