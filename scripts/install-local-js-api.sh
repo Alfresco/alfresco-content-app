01a82132a79e6181fa5e0957b02fe4118975148c
Martin Mueller <damadden88@googlemail.com>
2020-01-03 16:01:21 +0100

    include alfresco-js-api

--- scripts/install-local-js-api.sh
+++ scripts/install-local-js-api.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm -rf node_modules
 ./alfresco-js-api/scripts/build.sh
-rm -f node_modules/@alfresco/js-api
+npm install
+rm -rf node_modules/@alfresco/js-api
 mkdir -p node_modules/@alfresco/js-api
-cp alfresco-js-api/dist/package node_modules/@alfresco/js-api
+cp -rf alfresco-js-api/dist/package/* node_modules/@alfresco/js-api
