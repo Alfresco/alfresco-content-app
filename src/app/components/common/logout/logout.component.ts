acdda231c40d29d1e09ae01a69e2e1ecd34055e4
pionnegru <cilibiu.bogdan@yahoo.com>
2020-01-07 20:28:35 +0200

    user menu options components

---
+++ src/app/components/common/logout/logout.component.ts
@@ -0,0 +1,44 @@
+/*!
+ * @license
+ * Alfresco Example Content Application
+ *
+ * Copyright (C) 2005 - 2019 Alfresco Software Limited
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppStore, SetSelectedNodesAction } from '@alfresco/aca-shared/store';
+
+@Component({
+  selector: 'aca-logout',
+  template: `
+    <button mat-menu-item (click)="onLogoutEvent()" adf-logout>
+      {{ 'APP.SIGN_OUT' | translate }}
+    </button>
+  `
+})
+export class LogoutComponent {
+  constructor(private store: Store<AppStore>) {}
+
+  onLogoutEvent() {
+    this.store.dispatch(new SetSelectedNodesAction([]));
+  }
+}
