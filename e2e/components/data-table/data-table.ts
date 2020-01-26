d38efdf9fcfb98fac487ab66f6e1519e091487fb
Cilibiu Bogdan <bogdan.cilibiu@ness.com>
2020-01-26 09:28:46 +0200

    update e2e

--- e2e/components/data-table/data-table.ts
+++ e2e/components/data-table/data-table.ts
@@ -50,7 +50,7 @@ export class DataTable extends Component {
 
     selectedIcon: '.mat-icon[class*="selected"]',
     lockIcon: 'img[src*="lock"]',
-    lockOwner: '.aca-locked-by',
+    lockOwner: '.app-locked-by',
 
     emptyListContainer: 'div.adf-no-content-container',
     emptyFolderDragAndDrop: '.adf-empty-list_template .adf-empty-folder',
