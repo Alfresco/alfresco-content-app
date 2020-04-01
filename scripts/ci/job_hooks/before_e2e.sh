19a65dd286b469121a13127a6f602d2a8de2d0c9
maurizio vitale <maurizio.vitale@alfresco.com>
2020-04-01 14:08:00 +0100

    Try the new strategy

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -4,4 +4,4 @@ FROM=$1;
 TO=$2;
 
 ./scripts/ci/utils/artifact-from-s3.sh -a "$FROM" -o "$TO"
-# node "./scripts/app-config-replace.js" --config="$TO/app.config.json" -a
+node "./scripts/app-config-replace.js" --config="$TO/app.config.json" -a
