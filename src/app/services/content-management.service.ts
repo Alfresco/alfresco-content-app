2b11179951ceb91f9f44a919ec14b8a43dfd4c58
Cilibiu Bogdan <bogdan.cilibiu@ness.com>
2020-04-08 12:43:23 +0300

    strict immutability fix

--- src/app/services/content-management.service.ts
+++ src/app/services/content-management.service.ts
@@ -107,10 +107,13 @@ export class ContentManagementService {
   addFavorite(nodes: Array<MinimalNodeEntity>) {
     if (nodes && nodes.length > 0) {
       this.contentApi.addFavorite(nodes).subscribe(() => {
-        nodes.forEach(node => {
-          node.entry.isFavorite = true;
+        const favoriteNodes = nodes.map(node => {
+          const newNode = JSON.parse(JSON.stringify(node));
+          newNode.entry.isFavorite = true;
+          return newNode;
         });
-        this.store.dispatch(new SetSelectedNodesAction(nodes));
+
+        this.store.dispatch(new SetSelectedNodesAction(favoriteNodes));
       });
     }
   }
@@ -118,10 +121,12 @@ export class ContentManagementService {
   removeFavorite(nodes: Array<MinimalNodeEntity>) {
     if (nodes && nodes.length > 0) {
       this.contentApi.removeFavorite(nodes).subscribe(() => {
-        nodes.forEach(node => {
-          node.entry.isFavorite = false;
+        const favoriteNodes = nodes.map(node => {
+          const newNode = JSON.parse(JSON.stringify(node));
+          newNode.entry.isFavorite = false;
+          return newNode;
         });
-        this.store.dispatch(new SetSelectedNodesAction(nodes));
+        this.store.dispatch(new SetSelectedNodesAction(favoriteNodes));
       });
     }
   }
