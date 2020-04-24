23d7254d409913f2f8744eef13178ebc5b5f1c26
Eugenio Romano <eugenio.romano@alfresco.com>
2020-04-24 13:50:50 +0100

    install script fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ "${TRAVIS_BRANCH}" != "master" ]; then
-   ./update-version.sh -v alpha
+   ./scripts/update-version.sh -v alpha
 fi
