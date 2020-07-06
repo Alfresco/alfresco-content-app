a1f712cfa5b2d01456b44b08ce83731905f6b225
maurizio vitale <maurizio.vitale@alfresco.com>
2020-07-06 20:13:29 +0100

    Use hardcoded chrome version

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -2,8 +2,15 @@
 
 FROM=$1;
 TO=$2;
+PARAMS=$3;
 
+echo "Download artefact from S3 $FROM"
 ./scripts/ci/utils/artifact-from-s3.sh -a "$FROM" -o "$TO"
-node "./scripts/app-config-replace.js" --config="$TO/app.config.json" -a
+echo "Download artefact done"
 
+echo "Replace app.config with options $PARAMS"
+node "./scripts/app-config-replace.js" --config="$TO/app.config.json" $PARAMS
+echo "Replace app.config done"
+
+echo "Update update-webdriver"
 ./scripts/update-webdriver.sh
