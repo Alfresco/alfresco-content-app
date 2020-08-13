3edf48f050136001e6801b396ba93b7bac83d9bd
Popovics András <popovicsandras@users.noreply.github.com>
2020-08-13 23:20:05 +0200

    [AAE-3278] Temporary fix for some flakyness: C297638 and C297638 (#1602)

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -4,13 +4,12 @@ FROM=$1;
 TO=$2;
 PARAMS=$3;
 
-echo "Download artefact from S3 $FROM"
+# Download built application artifact from S3
 ./scripts/ci/utils/artifact-from-s3.sh -a "$FROM" -o "$TO"
-echo "Download artefact done"
-
-echo "Replace app.config with options $PARAMS"
 node "./scripts/app-config-replace.js" --config="$TO/app.config.json" $PARAMS
-echo "Replace app.config done"
+
+# Download protractor-smartrunner artifact related to this particular job from S3, if exists
+./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
 
 echo "====== Update webdriver-manager ====="
 if [ "$CI" = "true" ]; then
--- scripts/ci/job_hooks/before_install.sh
+++ scripts/ci/job_hooks/before_install.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
-# Run the build on mergin to development with always the latest ADF
-if [ "${TRAVIS_BRANCH}" == "develop" ] && [ "${TRAVIS_EVENT_TYPE}" == "api" ] || [ "${TRAVIS_EVENT_TYPE}" == "cron" ]  || [ "${TRAVIS_EVENT_TYPE}" == "push" ]; then
+# Run the build on develop branch with always the latest ADF
+if [[ "${TRAVIS_BRANCH}" == "develop" && ( "${TRAVIS_EVENT_TYPE}" == "api" || "${TRAVIS_EVENT_TYPE}" == "cron" || "${TRAVIS_EVENT_TYPE}" == "push" ) ]]; then
+    echo " ===== USING LATEST ADF VERSION ===== "
    ./scripts/update-version.sh -v $ADF_RELEASE_VERSION
 fi
 
-pip install --user awscli
-
+# Settings for Angular builder --------------------------------------------------------
 export NODE_OPTIONS="--max_old_space_size=30000"
+
+# Settings for protractor-smartrunner -------------------------------------------------
 export GIT_HASH=`git rev-parse HEAD`
-S3_DBP_PATH="s3://alfresco-travis-builds/aca"
+export SMART_RUNNER_DIRECTORY=".protractor-smartrunner"
+
+# Settings for Nx ---------------------------------------------------------------------
 export BASE_HASH="$(git describe --tags `git rev-list --tags --max-count=1`)"
 export HEAD_HASH="HEAD"
 
+# Settings for S3 caching -------------------------------------------------------------
+pip install --user awscli
+S3_DBP_PATH="s3://alfresco-travis-builds/aca"
 if [ "${TRAVIS_EVENT_TYPE}" == "push" ]; then
-    export S3_DBP_FOLDER="$S3_DBP_PATH/$TRAVIS_BRANCH/$TRAVIS_BUILD_ID"
+    export S3_DBP_ROOT_FOLDER="$S3_DBP_PATH/$TRAVIS_BRANCH"
 elif [ "${TRAVIS_EVENT_TYPE}" == "pull_request" ]; then
-    export S3_DBP_FOLDER="$S3_DBP_PATH/$TRAVIS_PULL_REQUEST/$TRAVIS_BUILD_ID"
+    export S3_DBP_ROOT_FOLDER="$S3_DBP_PATH/$TRAVIS_PULL_REQUEST"
     export BASE_HASH="origin/$TRAVIS_BRANCH"
 elif [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; then
-    export S3_DBP_FOLDER="$S3_DBP_PATH/cron/$TRAVIS_BUILD_ID"
+    export S3_DBP_ROOT_FOLDER="$S3_DBP_PATH/cron"
 else
-    export S3_DBP_FOLDER="$S3_DBP_PATH/api/$TRAVIS_BUILD_ID"
+    export S3_DBP_ROOT_FOLDER="$S3_DBP_PATH/api"
 fi
+export S3_DBP_FOLDER="$S3_DBP_ROOT_FOLDER/$TRAVIS_BUILD_ID"
 
 echo "BASE_HASH: $BASE_HASH"
+echo "S3 DBP root folder: $S3_DBP_ROOT_FOLDER"
 echo "S3 DBP destination: $S3_DBP_FOLDER"
-
-
