892ce28fa02a4cc2e9ebcd1f5d82583ef3cbacae
Adina Parpalita <Adina.Parpalita@ness.com>
2020-09-07 23:48:57 +0300

    move logs

--- scripts/ci/job_hooks/after_e2e.sh
+++ scripts/ci/job_hooks/after_e2e.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo "get docker logs after run"
+echo "--------------------------------"
+docker logs `docker ps | grep 'alfresco/alfresco-content-repository-community:latest' | awk '{ print $1 }'`
+echo "--------------------------------"
+
 # Upload protractor-smartrunner artifact related to this particular job to S3
 ./scripts/ci/utils/artifact-to-s3.sh -a "$SMART_RUNNER_DIRECTORY" -o "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2"
