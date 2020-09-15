500b0c6002124f17d7371c5f2992657b5a870740
Adina Parpalita <Adina.Parpalita@ness.com>
2020-09-15 23:57:54 +0300

    lint

--- e2e/suites/list-views/trash.test.ts
+++ e2e/suites/list-views/trash.test.ts
@@ -30,27 +30,27 @@ describe('Trash', () => {
 
   const siteName = `site-${Utils.random()}`;
   const fileSite = `file-${Utils.random()}.txt`;
-  let fileSiteId;
+  let fileSiteId: string;
 
   const folderAdmin = `folder-${Utils.random()}`;
-  let folderAdminId;
+  let folderAdminId: string;
   const fileAdmin = `file-${Utils.random()}.txt`;
-  let fileAdminId;
+  let fileAdminId: string;
 
   const folderUser = `folder-${Utils.random()}`;
-  let folderUserId;
+  let folderUserId: string;
   const fileUser = `file-${Utils.random()}.txt`;
-  let fileUserId;
+  let fileUserId: string;
 
   const folderDeleted = `folder-${Utils.random()}`;
-  let folderDeletedId;
+  let folderDeletedId: string;
   const fileDeleted = `file-${Utils.random()}.txt`;
-  let fileDeletedId;
+  let fileDeletedId: string;
 
   const folderNotDeleted = `folder-${Utils.random()}`;
   let folderNotDeletedId: string;
   const fileInFolder = `file-${Utils.random()}.txt`;
-  let fileInFolderId;
+  let fileInFolderId: string;
 
   const apis = {
     admin: new RepoClient(),
@@ -61,7 +61,7 @@ describe('Trash', () => {
   const page = new BrowsingPage();
   const { dataTable, breadcrumb } = page;
 
-  beforeAll(async (done) => {
+  beforeAll(async () => {
     await apis.admin.people.createUser({ username });
     fileAdminId = (await apis.admin.nodes.createFiles([fileAdmin])).entry.id;
     folderAdminId = (await apis.admin.nodes.createFolders([folderAdmin])).entry.id;
@@ -80,8 +80,6 @@ describe('Trash', () => {
     await apis.user.nodes.deleteNodesById([fileSiteId, fileUserId, folderUserId, fileInFolderId], false);
     await apis.user.nodes.deleteNodeById(fileDeletedId, false);
     await apis.user.nodes.deleteNodeById(folderDeletedId, false);
-
-    done();
   });
 
   afterAll(async (done) => {
