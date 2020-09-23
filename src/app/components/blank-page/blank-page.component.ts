71557dcceeb1261a73f79aa813d24b7dd2daa1d3
deepak.paul <sushmitha.v@muraai.com>
2020-09-23 19:52:12 +0530

    [ACA-3971] Removed unit test file for blank-page component

--- src/app/components/blank-page/blank-page.component.ts
+++ src/app/components/blank-page/blank-page.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'aca-blank-page',
-  styleUrls: ['./blank-page.component.scss']
+  template: ``
 })
 export class BlankPageComponent {}
