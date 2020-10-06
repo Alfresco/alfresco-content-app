edeb0958fdb91805dc32bf55c3bf6d65c2f73eec
Denys Vuika <denys.vuika@gmail.com>
2020-10-06 12:25:45 +0100

    add "create menu" unit tests

--- src/app/components/create-menu/create-menu.component.spec.ts
+++ src/app/components/create-menu/create-menu.component.spec.ts
@@ -24,9 +24,75 @@
  */
 
 import { CreateMenuComponent } from './create-menu.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CoreModule } from '@alfresco/adf-core';
+import { AppCreateMenuModule } from './create-menu.module';
+import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { AppExtensionService } from '@alfresco/aca-shared';
+import { ContentActionType } from '@alfresco/adf-extensions';
+import { By } from '@angular/platform-browser';
+import { AppTestingModule } from '../../testing/app-testing.module';
 
 describe('CreateMenuComponent', () => {
-  it('should be defined', () => {
-    expect(CreateMenuComponent).toBeDefined();
+  let fixture: ComponentFixture<CreateMenuComponent>;
+  let extensionService: AppExtensionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppTestingModule, CoreModule.forRoot(), AppCreateMenuModule, OverlayModule],
+      providers: [Overlay]
+    });
+
+    extensionService = TestBed.inject(AppExtensionService);
+    spyOn(extensionService, 'getCreateActions').and.returnValue([
+      {
+        id: 'action1',
+        type: ContentActionType.button,
+        title: 'action one'
+      }
+    ]);
+
+    fixture = TestBed.createComponent(CreateMenuComponent);
+  });
+
+  it('should render collapsed button', async () => {
+    fixture.componentInstance.expanded = false;
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="create-button"]')).nativeElement;
+    const isCollapsed = button.classList.contains('app-create-menu--collapsed');
+
+    expect(isCollapsed).toBe(true);
+  });
+
+  it('should render expanded button', async () => {
+    fixture.componentInstance.expanded = true;
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="create-button"]')).nativeElement;
+    const isCollapsed = button.classList.contains('app-create-menu--collapsed');
+
+    expect(isCollapsed).toBe(false);
+  });
+
+  it('should render custom actions', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(extensionService.getCreateActions).toHaveBeenCalled();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="create-button"]')).nativeElement;
+    button.click();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const menuItems = fixture.debugElement.queryAll(By.css('.app-toolbar-menu-item'));
+    expect(menuItems.length).toBe(1);
+
+    const label: HTMLSpanElement = (menuItems[0].nativeElement as HTMLButtonElement).querySelector('[data-automation-id="button-title"]');
+    expect(label.innerText).toBe('action one');
   });
 });
