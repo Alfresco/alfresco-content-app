9f66e6e3af49c7c4db5238883f4899cea718b8e4
Denys Vuika <denys.vuika@gmail.com>
2020-10-06 15:00:01 +0100

    submenu tests

--- src/app/components/create-menu/create-menu.component.spec.ts
+++ src/app/components/create-menu/create-menu.component.spec.ts
@@ -27,24 +27,27 @@ import { CreateMenuComponent } from './create-menu.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CoreModule } from '@alfresco/adf-core';
 import { AppCreateMenuModule } from './create-menu.module';
-import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { OverlayContainer, OverlayModule } from '@angular/cdk/overlay';
 import { AppExtensionService } from '@alfresco/aca-shared';
 import { ContentActionType } from '@alfresco/adf-extensions';
 import { By } from '@angular/platform-browser';
 import { AppTestingModule } from '../../testing/app-testing.module';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('CreateMenuComponent', () => {
   let fixture: ComponentFixture<CreateMenuComponent>;
   let extensionService: AppExtensionService;
+  let getCreateActionsSpy: jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AppTestingModule, CoreModule.forRoot(), AppCreateMenuModule, OverlayModule],
-      providers: [Overlay]
+      imports: [AppTestingModule, CoreModule.forRoot(), AppCreateMenuModule, OverlayModule, MatMenuModule, MatButtonModule]
     });
 
     extensionService = TestBed.inject(AppExtensionService);
-    spyOn(extensionService, 'getCreateActions').and.returnValue([
+    getCreateActionsSpy = spyOn(extensionService, 'getCreateActions');
+    getCreateActionsSpy.and.returnValue([
       {
         id: 'action1',
         type: ContentActionType.button,
@@ -54,6 +57,17 @@ describe('CreateMenuComponent', () => {
 
     fixture = TestBed.createComponent(CreateMenuComponent);
   });
+
+  async function clickMenu() {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="create-button"]')).nativeElement;
+    button.click();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+  }
 
   it('should render collapsed button', async () => {
     fixture.componentInstance.expanded = false;
@@ -78,21 +92,39 @@ describe('CreateMenuComponent', () => {
   });
 
   it('should render custom actions', async () => {
-    fixture.detectChanges();
-    await fixture.whenStable();
-
-    expect(extensionService.getCreateActions).toHaveBeenCalled();
-
-    const button: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="create-button"]')).nativeElement;
-    button.click();
-
-    fixture.detectChanges();
-    await fixture.whenStable();
+    await clickMenu();
 
     const menuItems = fixture.debugElement.queryAll(By.css('.app-toolbar-menu-item'));
     expect(menuItems.length).toBe(1);
 
-    const label: HTMLSpanElement = (menuItems[0].nativeElement as HTMLButtonElement).querySelector('[data-automation-id="button-title"]');
+    const label: HTMLSpanElement = (menuItems[0].nativeElement as HTMLButtonElement).querySelector('[data-automation-id="menu-item-title"]');
     expect(label.innerText).toBe('action one');
+  });
+
+  it('should render sub-menus', async () => {
+    getCreateActionsSpy.and.returnValue([
+      {
+        id: 'level1',
+        type: ContentActionType.menu,
+        title: 'level one',
+        children: [
+          {
+            id: 'level2',
+            type: ContentActionType.button,
+            title: 'level two'
+          }
+        ]
+      }
+    ]);
+
+    await clickMenu();
+
+    const level1 = fixture.debugElement.query(By.css('#level1')).nativeElement as HTMLButtonElement;
+    level1.click();
+
+    const overlayContainer = TestBed.inject(OverlayContainer);
+    const level2 = overlayContainer.getContainerElement().querySelector('#level2');
+
+    expect(level2).not.toBeNull();
   });
 });
