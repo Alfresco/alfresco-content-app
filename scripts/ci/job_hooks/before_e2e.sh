3a78506823a594125c2279b76b08a84db56292d6
Eugenio Romano <eugenio.romano@alfresco.com>
2020-11-19 10:29:08 +0000

    logs

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -4,9 +4,11 @@ FROM=$1;
 TO=$2;
 PARAMS=$3;
 
+echo "====== Check content UP ====="
 
 adf-cli check-cs-env --host "$API_CONTENT_HOST" -u "$ADMIN_EMAIL" -p "$ADMIN_PASSWORD" || exit 1
 
+echo "====== Download artifacts  ====="
 # Download built application artifact from S3
 ./scripts/ci/utils/artifact-from-s3.sh -a "$FROM" -o "$TO"
 node "./scripts/app-config-replace.js" --config="$TO/app.config.json" $PARAMS
