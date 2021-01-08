8af78c86f8ae845cf283a708505d518ac6ed865f
Thomas Hunter <thomas.hunter@alfresco.com>
2021-01-08 15:38:18 +0000

    [ACA-4232] Call the scan-env adf-cli command before and after each E2E job

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -4,6 +4,8 @@ FROM=$1;
 TO=$2;
 PARAMS=$3;
 
+npx @alfresco/adf-cli scan-env --host "$API_HOST" -u "$ADMIN_EMAIL" -p "$ADMIN_PASSWORD"
+
 echo "====== Check content UP ====="
 
 ./node_modules/@alfresco/adf-cli/bin/adf-cli check-cs-env --host "$API_CONTENT_HOST" -u "$ADMIN_EMAIL" -p "$ADMIN_PASSWORD" || exit 1
