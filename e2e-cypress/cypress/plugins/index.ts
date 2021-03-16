463e6662c300faedf1986a94c5abc933950f61b8
Adina Parpalita <Adina.Parpalita@ness.com>
2021-03-16 10:25:29 +0200

    read config params from env file

--- e2e-cypress/cypress/plugins/index.ts
+++ e2e-cypress/cypress/plugins/index.ts
@@ -6,12 +6,31 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
-  // No more webpack preprocessor here for TS support! Yay!
+// export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+//   // No more webpack preprocessor here for TS support! Yay!
 
-  on('task', {
-    // Define task functions
-  });
+//   on('task', {
+//     // Define task functions
+//   });
+
+//   return config;
+// };
+
+require('dotenv').config({ path: __dirname + `/../../../.env` });
+
+module.exports = (on, config) => {
+  config.baseUrl = process.env.E2E_HOST || 'http://localhost:4200';
+
+  config.env.params = {
+    config: {
+      hostEcm: process.env.API_CONTENT_HOST || 'http://localhost:8080',
+      providers: 'ECM',
+      authType: 'BASIC'
+    },
+    ADMIN_USERNAME: process.env.ADMIN_EMAIL || 'admin',
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin',
+    e2eRootPath: './'
+  };
 
   return config;
 };
