9f7bbeec3f23779a5ea914dce53cad3ca9620b72
iuliaib <iulia.burca@ness.com>
2021-03-18 13:47:04 +0200

    Delete video recording of passing specs

--- e2e-cypress/cypress/plugins/index.ts
+++ e2e-cypress/cypress/plugins/index.ts
@@ -17,6 +17,7 @@
 // };
 
 require('dotenv').config({ path: __dirname + `/../../../.env` });
+const del = require('del')
 
 module.exports = (on, config) => {
   config.baseUrl = process.env.E2E_HOST || 'http://localhost:4200';
@@ -32,5 +33,13 @@ module.exports = (on, config) => {
     e2eRootPath: './'
   };
 
+  //delete recordings of passing specs
+  on('after:spec', (spec, results) => {
+    if (results.stats.failures === 0 && results.video) {
+      // `del()` returns a promise, so it's important to return it to ensure deleting the video is finished before moving on
+      return del(results.video)
+    }
+  })
+  
   return config;
 };
