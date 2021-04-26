f6aa18d003be55e236cab0e5db532f1515c07b32
Maurizio Vitale <maurizio.vitale@alfresco.com>
2021-04-26 17:17:13 +0100

    Run the unit/build/e2e/docker based on the agreed doc

--- scripts/travis/deploy/publish.sh
+++ scripts/travis/deploy/publish.sh
@@ -10,7 +10,7 @@ npm run build.release
 TAG_VERSION=$(./scripts/travis/deploy/get-docker-image-tag-name.sh)
 echo "Running the docker with tag" $TAG_VERSION
 DOCKER_PROJECT_ARGS="PROJECT_NAME=$PROJECT_AFFECTED"
-
+DOCKER_REPOSITORY="$DOCKER_REPOSITORY_DOMAIN/$REPO_SLUG"
 # Publish Image to docker
 echo "npx @lfresco/adf-cli docker-publish --loginCheck --loginUsername '$DOCKER_REPOSITORY_USER' --loginPassword '$DOCKER_REPOSITORY_PASSWORD' --loginRepo '$DOCKER_REPOSITORY_DOMAIN' --dockerRepo '$DOCKER_REPOSITORY' --buildArgs $DOCKER_PROJECT_ARGS --dockerTags '$TAG_VERSION,$TRAVIS_BRANCH' "
 npx @lfresco/adf-cli docker-publish --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAG_VERSION,$TRAVIS_BRANCH" --pathProject "$(pwd)"
