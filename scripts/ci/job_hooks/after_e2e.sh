26afe58d2a0e0b89805d2cb026cce4b8637faf2f
Thomas Hunter <thomas.hunter@alfresco.com>
2021-04-30 12:58:38 +0200

    [ACS-1524] Remove environment scan (#2096)
    * [ACS-1524] Only run scan environment on cron
    
    * Temporary add branch to test cron
    
    * Remove temporary branch from .travis.yml
    
    * [ACS-1524] Remove environment scan

--- scripts/ci/job_hooks/after_e2e.sh
+++ scripts/ci/job_hooks/after_e2e.sh
@@ -2,5 +2,3 @@
 
 # Upload protractor-smartrunner artifact related to this particular job to S3
 ./scripts/ci/utils/artifact-to-s3.sh -a "$SMART_RUNNER_DIRECTORY" -o "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2"
-
-./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$APP_CONFIG_ECM_HOST" -u "$ADMIN_EMAIL" -p "$ADMIN_PASSWORD"
