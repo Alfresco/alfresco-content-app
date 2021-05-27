d47a401207dcc93897b672bb7486a14451f4c13a
Adina Parpalita <Adina.Parpalita@ness.com>
2021-05-27 12:27:22 +0300

    try to pin to specific chromedriver version

--- scripts/update-webdriver.sh
+++ scripts/update-webdriver.sh
@@ -3,8 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$CI" = "true" ]; then
     echo "Updating wedriver-manager with chromedriver: $npm_package_config_chromeDriver."
-    ./node_modules/protractor/bin/webdriver-manager update --gecko=false --versions.chrome=83.0.4103.116
+    ./node_modules/protractor/bin/webdriver-manager update --gecko=false --versions.chrome=90.0.4430.24
 else
     echo "Updating wedriver-manager with latest chromedriver, be sure to use evergreen Chrome."
-    ./node_modules/protractor/bin/webdriver-manager update --gecko=false
+    ./node_modules/protractor/bin/webdriver-manager update --gecko=false --versions.chrome=90.0.4430.24
 fi
