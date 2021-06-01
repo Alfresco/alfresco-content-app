361d1c2ddb90d62d7a75d7929585135671aa0be7
Eugenio Romano <eugenio.romano@alfresco.com>
2021-06-01 16:54:59 +0100

    update adf version

--- e2e/suites/extensions/ext-viewer.test.ts
+++ e2e/suites/extensions/ext-viewer.test.ts
@@ -23,16 +23,7 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-  AdminActions,
-  LoginPage,
-  BrowsingPage,
-  Viewer,
-  RepoClient,
-  EXTENSIBILITY_CONFIGS,
-  FILES,
-  Utils
-} from '@alfresco/aca-testing-shared';
+import { AdminActions, LoginPage, BrowsingPage, Viewer, RepoClient, EXTENSIBILITY_CONFIGS, FILES, Utils } from '@alfresco/aca-testing-shared';
 import { BrowserActions } from '@alfresco/adf-testing';
 
 describe('Extensions - Viewer', () => {
