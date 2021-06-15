ed4d40366a1aea3291a95af25858c957f1c3ae74
Vito Albano <vito.albano@alfresco.com>
2021-06-15 16:47:31 +0100

    [ACA-4471] - readded action

--- e2e/suites/actions-available/files-folders/test-data.ts
+++ e2e/suites/actions-available/files-folders/test-data.ts
@@ -383,11 +383,12 @@ const searchDocxContextMenu = [
   'Permissions'
 ];
 const searchToolbarPrimary = ['Toggle search filter', 'Share', 'Download', 'View', 'View Details', 'More Actions'];
-const searchSharedToolbarPrimary = ['Toggle search filter', 'Shared Link Settings', 'Download', 'View Details', 'More Actions'];
+const searchSharedToolbarPrimary = ['Toggle search filter', 'Shared Link Settings', 'Download', 'View', 'View Details', 'More Actions'];
 const searchFavLockedToolbarMore = ['Cancel Editing', 'Upload New Version', 'Remove Favorite', 'Copy', 'Manage Versions', 'Permissions'];
 const searchSharedFavLockedContextMenu = [
   'Shared Link Settings',
   'Download',
+  'View',
   'Cancel Editing',
   'Upload New Version',
   'Remove Favorite',
