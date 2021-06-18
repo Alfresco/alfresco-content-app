8cd58f5211e79e42d440c40868cac8cdb82ad4de
Denys Vuika <denys.vuika@gmail.com>
2021-06-18 15:12:23 +0100

    cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -10,8 +10,6 @@ module.exports = function(config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      // require('karma-coverage-istanbul-reporter'),
-      // require('karma-mocha-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
