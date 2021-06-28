92d447ec586f152c7823acf780174de8f19ff39f
Maurizio Vitale <maurizio.vitale@alfresco.com>
2021-06-28 11:03:48 +0100

    Fix @alfresco (#2202)

--- scripts/travis/deploy/publish.sh
+++ scripts/travis/deploy/publish.sh
@@ -12,5 +12,5 @@ echo "Running the docker with tag" $TAG_VERSION
 DOCKER_PROJECT_ARGS="PROJECT_NAME=$PROJECT_AFFECTED"
 DOCKER_REPOSITORY="$DOCKER_REPOSITORY_DOMAIN/$REPO_SLUG"
 # Publish Image to docker
-echo "npx @lfresco/adf-cli docker-publish --loginCheck --loginUsername '$DOCKER_REPOSITORY_USER' --loginPassword '$DOCKER_REPOSITORY_PASSWORD' --loginRepo '$DOCKER_REPOSITORY_DOMAIN' --dockerRepo '$DOCKER_REPOSITORY' --buildArgs $DOCKER_PROJECT_ARGS --dockerTags '$TAG_VERSION,$TRAVIS_BRANCH' "
-npx @lfresco/adf-cli docker-publish --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAG_VERSION,$TRAVIS_BRANCH" --pathProject "$(pwd)"
+echo "npx @alfresco/adf-cli docker-publish --loginCheck --loginUsername '$DOCKER_REPOSITORY_USER' --loginPassword '$DOCKER_REPOSITORY_PASSWORD' --loginRepo '$DOCKER_REPOSITORY_DOMAIN' --dockerRepo '$DOCKER_REPOSITORY' --buildArgs $DOCKER_PROJECT_ARGS --dockerTags '$TAG_VERSION,$TRAVIS_BRANCH' "
+npx @alfresco/adf-cli docker-publish --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAG_VERSION,$TRAVIS_BRANCH" --pathProject "$(pwd)"
