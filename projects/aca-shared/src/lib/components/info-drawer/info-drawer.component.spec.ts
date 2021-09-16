35e19ce2036309d980663b71b8cccd303ec2714e
Popovics András <popovicsandras@users.noreply.github.com>
2021-09-16 21:15:46 +0200

    [ci:force]

--- projects/aca-shared/src/lib/components/info-drawer/info-drawer.component.spec.ts
+++ projects/aca-shared/src/lib/components/info-drawer/info-drawer.component.spec.ts
@@ -22,6 +22,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
+
 import { ContentActionRef, SidebarTabRef } from '@alfresco/adf-extensions';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
