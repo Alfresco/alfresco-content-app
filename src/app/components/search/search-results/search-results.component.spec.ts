07709413630d66c0c33bfae85ac68c381b6ba114
Ketevani Kvirikashvili <qqetikv@gmail.com>
2021-10-27 13:27:41 +0300

    [ACA-4499] Search url in ACA is added (#2284)
    * [ACA-4499] Search url in ACA is added
    
    * fixed unit test
    
    * fixed
    
    * unit test is added
    
    * fix

--- src/app/components/search/search-results/search-results.component.spec.ts
+++ src/app/components/search/search-results/search-results.component.spec.ts
@@ -164,6 +164,16 @@ describe('SearchComponent', () => {
     expect(component.formatSearchQuery(query)).toBe(query);
   });
 
+  it('should be able to search if search input contains https url', () => {
+    const query = component.formatSearchQuery('https://alfresco.com');
+    expect(query).toBe(`(cm:name:"https://alfresco.com*")`);
+  });
+
+  it('should be able to search if search input contains http url', () => {
+    const query = component.formatSearchQuery('http://alfresco.com');
+    expect(query).toBe(`(cm:name:"http://alfresco.com*")`);
+  });
+
   it('should use original user input if text contains quotes', () => {
     const query = `"Hello World"`;
     expect(component.formatSearchQuery(query)).toBe(query);
