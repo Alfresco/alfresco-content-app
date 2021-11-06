bb5dda1a88668c512d1b9d2b9a5e571216a74f90
Denys Vuika <denys.vuika@alfresco.com>
2021-11-06 13:02:03 +0000

    add changelog report generation (#2342)

--- scripts/generate-reports.sh
+++ scripts/generate-reports.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 npx @alfresco/adf-cli@latest audit -d $DIR/docs/audit
 npx @alfresco/adf-cli@latest licenses -d $DIR/docs/licences
+npx @alfresco/adf-cli@alpha changelog -o $DIR/docs/changelog
