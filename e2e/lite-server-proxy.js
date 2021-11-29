5f8978a9179a2744699fc9234913bfbffee759c2
Maurizio Vitale <maurizio.vitale@alfresco.com>
2021-11-29 18:19:59 +0000

    Use the same proxy strategy for e2e

--- e2e/lite-server-proxy.js
+++ e2e/lite-server-proxy.js
@@ -1,5 +1,43 @@
 let fallback = require('connect-history-api-fallback');
+require('@alfresco/adf-cli/tooling').dotenvConfig();
+var proxyMiddleware = require('http-proxy-middleware');
 
+function getProxy(contentHost) {
+  return {
+      '/alfresco': {
+          target: contentHost,
+          secure: false,
+          changeOrigin: true,
+          // workaround for REPO-2260
+          onProxyRes: function (proxyRes, req, res) {
+              const header = proxyRes.headers['www-authenticate'];
+              if (header && header.startsWith('Basic')) {
+                  proxyRes.headers['www-authenticate'] = 'x' + header;
+              }
+          },
+      }
+  }
+}
+
+function getMiddleWares() {
+  var counter = 0;
+  const proxies = {
+      ...getProxy(process.env.APP_CONFIG_ECM_HOST),
+  };
+  const appMiddlewares = Object.keys(proxies).reduce((accumulator, proxyName) => {
+      counter++;
+      return {
+          ...accumulator,
+          [counter]: proxyMiddleware(proxyName, proxies[proxyName]),
+      }
+  }, {});
+  counter++;
+
+  return {
+      ...appMiddlewares,
+      [counter]: fallback({ index: '/index.html', verbose: true }),
+  };
+}
 module.exports = {
   injectChanges: false, // workaround for Angular 2 styleUrls loading
   files: ['./**/*.{html,htm,css,js}'],
@@ -11,8 +49,6 @@ module.exports = {
   port: 4200,
   open: false,
   server: {
-    middleware: {
-      1: fallback({ index: '/index.html', verbose: true })
-    }
+    middleware: getMiddleWares(),
   }
 };
