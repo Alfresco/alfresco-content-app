056ff4b011b6bf56b4f85a253534c2b378a76970
Maurizio Vitale <maurizio.vitale@alfresco.com>
2021-12-07 15:38:01 +0000

    [AAE-6455] use tpl format instead of json and replace values (#2374)
    * Use the tpl instead of json and replace values for build start and e2e
    
    * Use different license file name to avoid conflicts
    
    * use the travis value
    
    * Use the general ng build and add doc

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# ---------------------------------------------------------------
+# Validating replaced app.config.json
+# ---------------------------------------------------------------
+app_config_checker(){
+    APP_CONFIG_FILE_PATH=$1;
+
+    echo  -n "     \_ Validating replaced config file ... ";
+        $(npm bin)/ajv validate -s ./node_modules/@alfresco/adf-core/app.config.schema.json -d $APP_CONFIG_FILE_PATH --errors=text --verbose || exit 4
+
+        if grep -E -q '\$\{[A-Z0-9_]*\}' $APP_CONFIG_FILE_PATH; then
+            echo -e "\e[31m        \_ ERROR: Variables are still present in the app.config.json file. Some of them might not have default value set.\e[0m";
+            exit 5;
+        fi
+}
+
 FROM=$1;
 TO=$2;
 PARAMS=$3;
@@ -11,7 +26,13 @@ echo "====== Check content UP ====="
 echo "====== Download artifacts ====="
 # Download built application artifact from S3
 ./scripts/ci/utils/artifact-from-s3.sh -a "$FROM" -o "$TO"
-node "./scripts/app-config-replace.js" --config="$TO/app.config.json" $PARAMS
+
+APP_CONFIG_FILE_PATH="$TO/app.config.json"
+EXTRA_ENV_SETTINGS=""
+# Replace variables in app.config.json
+envsub $EXTRA_ENV_SETTINGS --all $APP_CONFIG_FILE_PATH $APP_CONFIG_FILE_PATH || exit 1
+
+app_config_checker $APP_CONFIG_FILE_PATH
 
 # Download protractor-smartrunner artifact related to this particular job from S3, if exists
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,23 +1,15 @@
-require('dotenv').config();
+require('@alfresco/adf-cli/tooling').dotenvConfig({ path: process.env.ENV_FILE });
 
-const APP_CONFIG_ECM_HOST = process.env.APP_CONFIG_ECM_HOST || 'http://0.0.0.0:8080';
+const APP_CONFIG_ECM_HOST = process.env.APP_CONFIG_ECM_HOST;
 
 module.exports = {
-  '/alfresco': {
-    target: APP_CONFIG_ECM_HOST,
-    secure: false,
-    changeOrigin: true,
-    // workaround for REPO-2260
-    onProxyRes: function (proxyRes) {
-      const header = proxyRes.headers['www-authenticate'];
-      if (header && header.startsWith('Basic')) {
-        proxyRes.headers['www-authenticate'] = 'x' + header;
-      }
+    "/alfresco": {
+        "target": APP_CONFIG_ECM_HOST,
+        "secure": false,
+        "pathRewrite": {
+            "^/alfresco/alfresco": ""
+        },
+        "changeOrigin": true,
+        'logLevel': 'debug'
     }
-  },
-  '/auth': {
-    target: APP_CONFIG_ECM_HOST,
-    secure: false,
-    changeOrigin: true
-  }
 };
