3491739f1f10690c63bf4b9557fd5a46e0c8893e
Mykyta Maliarchuk <maliarchuk99@gmail.com>
2022-08-03 12:50:18 +0200

    created service for rules

---
+++ projects/aca-folder-rules/src/lib/services/folder-rules.service.spec.ts
@@ -0,0 +1,201 @@
+/*!
+ * @license
+ * Alfresco Example Content Application
+ *
+ * Copyright (C) 2005 - 2020 Alfresco Software Limited
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {FolderRulesService} from "./folder-rules.service";
+import {TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Rule} from "../model/rule.model";
+
+fdescribe('FolderRulesService', () => {
+
+  let folderRulesService: FolderRulesService,
+    httpTestingController: HttpTestingController
+
+  let dummyResponse = {
+    "list": {
+      "pagination": {
+        "count": 2,
+        "hasMoreItems": false,
+        "totalItems": 2,
+        "skipCount": 0,
+        "maxItems": 100
+      },
+      "entries": [
+        {
+          "entry": {
+            "shared": false,
+            "cascade": false,
+            "asynchronous": false,
+            "name": "rule1",
+            "id": "53273531-fb09-4bcc-97c9-dc1d2036275e",
+            "triggers": [
+              "INBOUND"
+            ],
+            "actions": [
+              {
+                "actionDefinitionId": "copy",
+                "params": {
+                  "deep-copy": false,
+                  "destination-folder": "6c98161d-2aa1-4f06-9b37-ed46be28320d",
+                  "actionContext": "rule"
+                }
+              }
+            ],
+            "enabled": true
+          }
+        },
+        {
+          "entry": {
+            "shared": false,
+            "cascade": false,
+            "asynchronous": false,
+            "name": "rule2",
+            "id": "b21fbf98-41da-4023-bc7e-2e727b3324d4",
+            "triggers": [
+              "INBOUND"
+            ],
+            "actions": [
+              {
+                "actionDefinitionId": "move",
+                "params": {
+                  "destination-folder": "6c98161d-2aa1-4f06-9b37-ed46be28320d",
+                  "actionContext": "rule"
+                }
+              }
+            ],
+            "enabled": true
+          }
+        }
+      ]
+    }
+  }
+  let dummyRules: Partial<Rule>[] = [
+    {
+      "shared": false,
+      "cascade": false,
+      "asynchronous": false,
+      "name": "rule1",
+      "id": "53273531-fb09-4bcc-97c9-dc1d2036275e",
+      "triggers": [
+        "INBOUND"
+      ],
+      "actions": [
+        {
+          "actionDefinitionId": "copy",
+          "params": {
+            "deep-copy": false,
+            "destination-folder": "6c98161d-2aa1-4f06-9b37-ed46be28320d",
+            "actionContext": "rule"
+          }
+        }
+      ],
+      "enabled": true
+    },
+    {
+      "shared": false,
+      "cascade": false,
+      "asynchronous": false,
+      "name": "rule2",
+      "id": "b21fbf98-41da-4023-bc7e-2e727b3324d4",
+      "triggers": [
+        "INBOUND"
+      ],
+      "actions": [
+        {
+          "actionDefinitionId": "move",
+          "params": {
+            "destination-folder": "6c98161d-2aa1-4f06-9b37-ed46be28320d",
+            "actionContext": "rule"
+          }
+        }
+      ],
+      "enabled": true
+    }
+  ]
+
+  let firstRuleId = dummyResponse['list']['entries'][0]['entry'].id
+
+  beforeEach((() => {
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        FolderRulesService
+      ]
+    })
+
+    folderRulesService = TestBed.inject<FolderRulesService>(FolderRulesService)
+    httpTestingController = TestBed.inject<HttpTestingController>(HttpTestingController)
+
+
+  }))
+
+  it('should set a list of rules to observable', (done: DoneFn) => {
+
+    folderRulesService.loadAllRules()
+
+    setTimeout(() => {
+
+      let rules$ = folderRulesService.rulesListing$
+
+      rules$.subscribe(
+        res => {
+
+          console.log(res)
+
+          expect(res).toBeTruthy('No response received')
+
+          expect(res).toEqual(dummyRules)
+
+          expect(res[0].id).toBe(firstRuleId)
+
+          done()
+
+        }
+      )
+    })
+
+
+    // rules$.toPromise().then((res) => {
+    //
+    //     console.log(res)
+    //
+    //     expect(res).toBeTruthy('No response received')
+    //
+    //     expect(res).toEqual(dummyRules)
+    //
+    //     expect(res[0].id).toBe(firstRuleId)
+    // })
+
+
+    const req = httpTestingController.expectOne(`https://acadev.envalfresco.com/alfresco/api/-default-/public/alfresco/versions/1/nodes/d91aa433-45f0-4c4c-9fb1-89ade91215aa/rule-sets/-default-/rules`)
+    expect(req.request.method).toEqual('GET')
+    req.flush(dummyResponse)
+
+  })
+
+
+})
+
---
+++ projects/aca-folder-rules/src/lib/services/folder-rules.service.ts
@@ -0,0 +1,91 @@
+/*!
+ * @license
+ * Alfresco Example Content Application
+ *
+ * Copyright (C) 2005 - 2020 Alfresco Software Limited
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, Observable} from "rxjs";
+import {finalize, map} from "rxjs/operators";
+import {Rule} from "../model/rule.model";
+import {AppConfigService} from "@alfresco/adf-core";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FolderRulesService {
+
+  baseUrl = 'https://acadev.envalfresco.com/alfresco/api/-default-/public/alfresco/versions/1'
+  nodeId = 'd91aa433-45f0-4c4c-9fb1-89ade91215aa'
+  identityHost = ''
+
+  private rulesListingSource = new BehaviorSubject<Partial<Rule>[]>([]);
+  rulesListing$: Observable<Partial<Rule>[]> = this.rulesListingSource.asObservable();
+  private loadingSource = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSource.asObservable();
+
+  // private appConfig: AppConfigService second parameter of constructor
+
+  constructor(private http: HttpClient, private appConfig: AppConfigService) {
+    this.identityHost = this.appConfig.get('ecmHost');
+  }
+
+  loadAllRules(nodeId: string = this.nodeId, ruleSetId: string = '-default-'): void {
+    this.loadingSource.next(true);
+    // ${this.appConfig.get()}
+    this.http.get(`${this.baseUrl}/nodes/${nodeId}/rule-sets/${ruleSetId}/rules`)
+      .pipe(
+        map(res => this.formatRules(res)),
+        finalize(() => this.loadingSource.next(false))
+      )
+      .subscribe(
+        res => this.rulesListingSource.next(res),
+        err => this.rulesListingSource.error(err)
+      )
+  }
+
+  formatRules(res): Rule[] {
+    return res.list.entries.map(entry => this.formatRule(entry.entry));
+  }
+
+  formatRule(obj): Rule {
+    return {
+      id: obj.id,
+      name: obj.name ?? '',
+      description: obj.description ?? '',
+      enabled: obj.enabled ?? true,
+      cascade: obj.cascade ?? false,
+      asynchronous: obj.asynchronous ?? false,
+      errorScript: obj.errorScript ?? '',
+      shared: obj.shared ?? false,
+      triggers: obj.triggers ?? ['INBOUND'],
+      conditions: obj.conditions ?? [],
+      actions: obj.actions ?? []
+    };
+  }
+
+}
+
+
+
