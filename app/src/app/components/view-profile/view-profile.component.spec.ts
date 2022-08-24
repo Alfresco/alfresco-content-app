8a227ca4b17fc09f6ebc9cb883df90bdb00c995a
SheenaMalhotra182 <Sheena.Malhotra@contractors.onbase.com>
2022-08-24 12:20:44 +0530

    Test Specs for ViewProfileComponent Added

--- app/src/app/components/view-profile/view-profile.component.spec.ts
+++ app/src/app/components/view-profile/view-profile.component.spec.ts
@@ -9,6 +9,7 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppConfigModule } from '@alfresco/adf-core';
 import { ViewProfileComponent } from './view-profile.component';
 import { AppTestingModule } from '../../testing/app-testing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('ViewProfileComponent', () => {
   let fixture: ComponentFixture<ViewProfileComponent>;
@@ -16,7 +17,7 @@ describe('ViewProfileComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AppTestingModule, AppConfigModule],
+      imports: [AppTestingModule, AppConfigModule, FormsModule, ReactiveFormsModule],
       declarations: [ViewProfileComponent]
     });
 
@@ -28,4 +29,51 @@ describe('ViewProfileComponent', () => {
     expect(component.loginSectionDropdown).toBe(false);
     expect(component.contactSectionDropdown).toBe(false);
   });
+
+  it('Save Button is Disabled if Profile Form has invalid inputs.', () => {
+    component.ngOnInit();
+    const profileFormGroup = component.profileForm;
+
+    profileFormGroup.setValue({
+      jobTitle: "Developer",
+      location: "Kolkata",
+      telephone: "27442ABC", //invalid format
+      mobile: "AB8866322112", //invalid format
+      oldPassword: "admin@123",
+      newPassword: "admin@1234",
+      verifyPassword: "admin@1234",
+      companyName: "Hyland",
+      companyPostCode: "12345",
+      companyAddress: "Hyland Software, Kolkata, West Bengal",
+      companyTelephone: "27442266",
+      companyEmail: "HylandContact" //invalid format
+    });
+
+    expect(profileFormGroup.valid).toEqual(false);
+    expect(component.isSaveButtonDisabled()).toBeTruthy();
+
+   });
+
+   it('Save Button is Enabled if Profile Form has valid inputs.', () => {
+    component.ngOnInit();
+    const profileFormGroup = component.profileForm;
+    profileFormGroup.setValue({
+      jobTitle: "Developer",
+      location: "Kolkata",
+      telephone: "27442255", //valid format
+      mobile: "8866322112", //valid format
+      oldPassword: "admin@123",
+      newPassword: "admin@1234",
+      verifyPassword: "admin@1234",
+      companyName: "Hyland",
+      companyPostCode: "12345",
+      companyAddress: "Hyland Software, Kolkata, West Bengal",
+      companyTelephone: "27442266", //valid format
+      companyEmail: "HylandContact@hyland.com" //valid format
+    });
+
+    expect(profileFormGroup.valid).toEqual(true);
+    expect(component.isSaveButtonDisabled()).toBeFalsy();
+
+   });
 });
--- app/src/app/material.module.ts
+++ app/src/app/material.module.ts
@@ -32,10 +32,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [MatMenuModule, MatIconModule, MatButtonModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatProgressBarModule, MatCardModule],
-  exports: [MatMenuModule, MatIconModule, MatButtonModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatProgressBarModule, MatCardModule],
+  imports: [MatMenuModule, MatIconModule, MatButtonModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatProgressBarModule, MatCardModule, MatDividerModule],
+  exports: [MatMenuModule, MatIconModule, MatButtonModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatProgressBarModule, MatCardModule, MatDividerModule],
   providers: [
     {
       provide: MAT_DIALOG_DEFAULT_OPTIONS,
