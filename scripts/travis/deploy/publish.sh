97a49e66b21e53f1735b0ac389022c9457d24d72
Mohinish Sah <Mohinish.Sah@contractors.onbase.com>
2022-12-09 14:26:35 +0530

    [ACA-4643] pushing changes to Alfresco dev repo

--- scripts/travis/deploy/publish.sh
+++ scripts/travis/deploy/publish.sh
@@ -14,7 +14,7 @@ npm ci && npm run build.release
 TAG_VERSION=$(./scripts/travis/deploy/get-docker-image-tag-name.sh)
 echo "Running the docker with tag" $TAG_VERSION
 DOCKER_PROJECT_ARGS="PROJECT_NAME=$PROJECT_AFFECTED"
-DOCKER_REPOSITORY="$DOMAIN/$REPO_SLUG"
+DOCKER_REPOSITORY="$DOMAIN/alfresco/dev"
 
 # Publish Image to quay.io or dockerhub or another domain - only publish the version on master - elsewhere version and branch
 if [[ $TRAVIS_BRANCH == "master" ]]; then
