c9498bb48f51481fcea2b0200c74abb3402272e0
Bartosz Sekula <Bartosz.Sekula@hyland.com>
2023-01-25 14:24:54 +0100

    update

--- scripts/ci/npm/publish-libs.sh
+++ scripts/ci/npm/publish-libs.sh
@@ -5,32 +5,41 @@ ROOT_DIR=${DIR}/../../..
 DIST_DIR=${ROOT_DIR}/dist/@alfresco
 PROJECTS_DIR=${ROOT_DIR}/projects
 
-for var in "$@"
-do
-    case "$var" in
-      --dryrun) DRY_RUN=true
-      echo -e "Run with --dryrun, therefore script won't publish packages\n" ;
-    esac
-done
+DRY_RUN=""
+if [ "$PUBLISH_PROJECTS" = "false" ]; then
+    DRY_RUN=true
+    echo "PUBLISH_PROJECTS is set to false, run in dry mode"
+fi
 
 VERSION_IN_PACKAGE_JSON=`node -p "require('$ROOT_DIR/package.json')".version;`;
 
-if [ "$TRAVIS_BRANCH" = "master" ] ; then
+if [[ $TRAVIS_BRANCH =~ ^master.*?$ ]] ; then
   NEW_LIBRARY_VERSION=VERSION_IN_PACKAGE_JSON
 else
   NEW_LIBRARY_VERSION="${VERSION_IN_PACKAGE_JSON}-${TRAVIS_BUILD_NUMBER}"
 fi
 
+if [[ $TRAVIS_BRANCH =~ ^master(-patch.*)?$ ]]
+then
+    # Pre-release versions
+    if [[ $VERSION_IN_PACKAGE_JSON =~ ^[0-9]*\.[0-9]*\.[0-9]*-A\.[0-9]*$ ]];
+    then
+        TAG_NPM=next
+    # Stable major versions
+    else
+        TAG_NPM=latest
+    fi
+fi
+
+if [[ $TRAVIS_BRANCH =~ ^develop(-patch.*)?$ ]]
+then
+    TAG_NPM=alpha
+fi
+
+echo -e "Branch is '$TRAVIS_BRANCH' therefore publish with '$TAG_NPM' tag\n"
+
 PROJECTS=(
     'aca-shared'
-    'aca-about'
-    'adf-office-services-ext'
-    'aca-preview'
-    'aca-viewer'
-    'aca-content'
-    'aca-folder-rules'
-    'aca-settings'
-    'aca-testing-shared'
 );
 
 for PROJECT in "${PROJECTS[@]}"
