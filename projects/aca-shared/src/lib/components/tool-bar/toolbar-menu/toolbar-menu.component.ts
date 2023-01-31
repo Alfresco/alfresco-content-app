f275c6d934ebe32c6e2b1efeb5ed7e762ac517c1
swapnil.verma <swapnil.verma@globallogic.com>
2023-01-31 12:51:35 +0530

    [ACS-4251] Added keyboard navigation for toolbar menu items

--- projects/aca-shared/src/lib/components/tool-bar/toolbar-menu/toolbar-menu.component.ts
+++ projects/aca-shared/src/lib/components/tool-bar/toolbar-menu/toolbar-menu.component.ts
@@ -23,9 +23,9 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, Input, ViewEncapsulation, HostListener, ViewChild, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import { Component, Input, ViewEncapsulation, HostListener, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { ContentActionRef } from '@alfresco/adf-extensions';
-import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { ThemePalette } from '@angular/material/core';
 import { ToolbarMenuItemComponent } from '../toolbar-menu-item/toolbar-menu-item.component';
 
@@ -35,7 +35,7 @@ import { ToolbarMenuItemComponent } from '../toolbar-menu-item/toolbar-menu-item
   encapsulation: ViewEncapsulation.None,
   host: { class: 'app-toolbar-menu' }
 })
-export class ToolbarMenuComponent implements AfterViewInit {
+export class ToolbarMenuComponent {
   @Input()
   actionRef: ContentActionRef;
 
@@ -56,20 +56,14 @@ export class ToolbarMenuComponent implements AfterViewInit {
     this.matTrigger.closeMenu();
   }
 
-  ngAfterViewInit(): void {
-    const menuItems: MatMenuItem[] = [];
-    this.toolbarMenuItems.forEach((toolbarMenuItem: ToolbarMenuItemComponent) => {
-      if (toolbarMenuItem.menuItem !== undefined) {
-        menuItems.push(toolbarMenuItem.menuItem);
-      }
-    });
-    const menuItemsQueryList: QueryList<MatMenuItem> = new QueryList<MatMenuItem>();
-    menuItemsQueryList.reset(menuItems);
-    this.menu._allItems = menuItemsQueryList;
-    this.menu.ngAfterContentInit();
-  }
-
   trackByActionId(_: number, obj: ContentActionRef): string {
     return obj.id;
+  }
+
+  onCustomItemContainerClick(event) {
+    console.log(event);
+    const el: HTMLElement = event.target;
+    const ev = new MouseEvent('click');
+    el.firstElementChild?.firstElementChild?.dispatchEvent(ev);
   }
 }
