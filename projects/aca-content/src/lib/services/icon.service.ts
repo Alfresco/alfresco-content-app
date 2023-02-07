ed9e647a7135efd7a33c9329d22dc0dbb8236ed2
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-02-07 20:37:03 +0000

    Layout changes for workspace sidemenu

---
+++ projects/aca-content/src/lib/services/icon.service.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2005 - 2021 Alfresco Software, Ltd. All rights reserved.
+ *
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
+import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IconService {
+  contentIcons: string[] = ['workspace', 'menu', 'avatar', 'settings'];
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {}
+  public registerIcons(): void {
+    this.loadIcons(this.contentIcons, './assets/svg/icons');
+  }
+  private loadIcons(iconKeys: string[], iconUrl: string): void {
+    iconKeys.forEach((key) => {
+      this.matIconRegistry.addSvgIcon(key, this.domSanitizer.bypassSecurityTrustResourceUrl(`${iconUrl}/${key}.svg`));
+    });
+  }
+}
