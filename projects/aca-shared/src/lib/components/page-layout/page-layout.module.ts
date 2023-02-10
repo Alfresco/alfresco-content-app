ee17bf93b80890e71ac2b90697e9c7a5b67e69bb
Denys Vuika <denys.vuika@gmail.com>
2023-02-10 17:22:46 -0500

    use only specific modules needed for page layout

--- projects/aca-shared/src/lib/components/page-layout/page-layout.module.ts
+++ projects/aca-shared/src/lib/components/page-layout/page-layout.module.ts
@@ -29,10 +29,11 @@ import { PageLayoutErrorComponent } from './page-layout-error.component';
 import { PageLayoutHeaderComponent } from './page-layout-header.component';
 import { PageLayoutComponent } from './page-layout.component';
 import { CommonModule } from '@angular/common';
-import { CoreModule } from '@alfresco/adf-core';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  imports: [CommonModule, CoreModule.forChild()],
+  imports: [CommonModule, MatIconModule, TranslateModule],
   declarations: [PageLayoutContentComponent, PageLayoutErrorComponent, PageLayoutHeaderComponent, PageLayoutComponent],
   exports: [PageLayoutContentComponent, PageLayoutErrorComponent, PageLayoutHeaderComponent, PageLayoutComponent]
 })
