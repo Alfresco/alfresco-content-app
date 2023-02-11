dc1b2847ae6473e509f2dceee583a9777bc62a25
Denys Vuika <denys.vuika@gmail.com>
2023-02-11 10:58:26 -0500

    deprecate "headerTextColor" customisation

--- projects/aca-content/src/lib/store/initial-state.ts
+++ projects/aca-content/src/lib/store/initial-state.ts
@@ -28,7 +28,6 @@ import { AppState, AppStore } from '@alfresco/aca-shared/store';
 export const INITIAL_APP_STATE: AppState = {
   appName: 'Alfresco Content Application',
   headerColor: '#ffffff',
-  headerTextColor: '#000000',
   logoPath: 'assets/images/alfresco-logo-white.svg',
   customCssPath: '',
   webFontPath: '',
