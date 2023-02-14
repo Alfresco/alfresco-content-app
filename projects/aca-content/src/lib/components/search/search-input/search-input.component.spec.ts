2b78a495701b7f55ea482d93dbad6c4b1ba96c27
Denys Vuika <denys.vuika@gmail.com>
2023-02-14 13:13:31 -0500

    code cleanup after reviews

--- projects/aca-content/src/lib/components/search/search-input/search-input.component.spec.ts
+++ projects/aca-content/src/lib/components/search/search-input/search-input.component.spec.ts
@@ -32,14 +32,14 @@ import { SearchByTermAction, SearchActionTypes, SnackbarErrorAction, SnackbarAct
 import { AppHookService } from '@alfresco/aca-shared';
 import { map } from 'rxjs/operators';
 import { SearchQueryBuilderService } from '@alfresco/adf-content-services';
-import { SearchInputService } from '../search-input.service';
+import { SearchNavigationService } from '../search-navigation.service';
 
 describe('SearchInputComponent', () => {
   let fixture: ComponentFixture<SearchInputComponent>;
   let component: SearchInputComponent;
   let actions$: Actions;
   let appHookService: AppHookService;
-  let searchInputService: SearchInputService;
+  let searchInputService: SearchNavigationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -52,7 +52,7 @@ describe('SearchInputComponent', () => {
     actions$ = TestBed.inject(Actions);
     fixture = TestBed.createComponent(SearchInputComponent);
     appHookService = TestBed.inject(AppHookService);
-    searchInputService = TestBed.inject(SearchInputService);
+    searchInputService = TestBed.inject(SearchNavigationService);
     component = fixture.componentInstance;
   });
 
@@ -234,7 +234,7 @@ describe('SearchInputComponent', () => {
     it('should exit search on click of close icon', async () => {
       spyOn(searchInputService, 'isSearchRoute').and.returnValue(true);
       spyOn(component, 'exitSearch').and.callThrough();
-      spyOn(searchInputService, 'exitSearch').and.callThrough();
+      spyOn(searchInputService, 'navigateBack').and.callThrough();
 
       fixture.detectChanges();
       await fixture.whenStable();
@@ -246,7 +246,7 @@ describe('SearchInputComponent', () => {
       await fixture.whenStable();
 
       expect(component.exitSearch).toHaveBeenCalled();
-      expect(searchInputService.exitSearch).toHaveBeenCalledWith();
+      expect(searchInputService.navigateBack).toHaveBeenCalledWith();
     });
   });
 });
