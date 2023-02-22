8b25f31587b553d34b0d6387fefaafdcc80f5610
MichalKinas <michal.kinas@hyland.com>
2023-02-22 17:25:41 +0100

    [ACA-4652] Disable failing download on Travis, npm ci in before e2e

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -35,7 +35,7 @@ envsub $EXTRA_ENV_SETTINGS --all $APP_CONFIG_FILE_PATH $APP_CONFIG_FILE_PATH || 
 app_config_checker $APP_CONFIG_FILE_PATH
 
 # Download protractor-smartrunner artifact related to this particular job from S3, if exists
-./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
+# ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
 
 echo "====== Update webdriver-manager ====="
 if [ "$CI" = "true" ]; then
