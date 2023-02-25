2c936e663d88aa6ade730a3bb126f3894b73e191
Denys Vuika <denys.vuika@gmail.com>
2023-02-25 16:09:45 -0500

    fix folder rules tests

--- projects/aca-folder-rules/src/lib/manage-rules/manage-rules.smart-component.spec.ts
+++ projects/aca-folder-rules/src/lib/manage-rules/manage-rules.smart-component.spec.ts
@@ -29,7 +29,7 @@ import { DebugElement, Predicate } from '@angular/core';
 import { CoreTestingModule } from '@alfresco/adf-core';
 import { FolderRulesService } from '../services/folder-rules.service';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { BehaviorSubject, of, Subject } from 'rxjs';
 import {
   inheritedRuleSetMock,
   inheritedRuleSetWithEmptyRulesMock,
@@ -44,6 +44,7 @@ import { ActionsService } from '../services/actions.service';
 import { FolderRuleSetsService } from '../services/folder-rule-sets.service';
 import { ruleMock, ruleSettingsMock } from '../mock/rules.mock';
 import { Store } from '@ngrx/store';
+import { AppService } from '@alfresco/aca-shared';
 
 describe('ManageRulesSmartComponent', () => {
   let fixture: ComponentFixture<ManageRulesSmartComponent>;
@@ -58,6 +59,13 @@ describe('ManageRulesSmartComponent', () => {
     TestBed.configureTestingModule({
       imports: [CoreTestingModule, AcaFolderRulesModule],
       providers: [
+        {
+          provide: AppService,
+          useValue: {
+            appNavNarMode$: new BehaviorSubject('expanded'),
+            toggleAppNavBar$: new Subject()
+          }
+        },
         FolderRuleSetsService,
         FolderRulesService,
         { provide: Store, useValue: { dispatch: () => {} } },
