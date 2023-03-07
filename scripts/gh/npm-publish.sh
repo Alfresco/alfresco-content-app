4933b1db42ba319604053ec219b9d6f55178d843
Denys Vuika <denys.vuika@hyland.com>
2023-03-07 11:12:57 -0500

    [ACA-4649] release workflow enhancements (#3043)
    * move update versions of libs to shell script
    
    * unify packaging script
    
    * update package.json repository
    
    * change github token

---
+++ scripts/gh/npm-publish.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+DIST_DIR="$ROOT_DIR/dist/@alfresco"
+TAG=$1
+DRY_RUN=$2
+
+if [[ -z "$TAG" ]]; then
+    echo "Missing tag parameter"
+    exit 1
+fi
+
+export PROJECTS=(
+  'aca-about'
+  'aca-content'
+  'aca-folder-rules'
+  'adf-office-services-ext'
+  'aca-preview'
+  'aca-shared'
+  'aca-viewer'
+);
+
+for PROJECT in ${PROJECTS[@]}
+do
+  cd $DIST_DIR/$PROJECT
+
+  if [[ "$DRY_RUN" == "true" ]]; then
+    echo "[DRY RUN] Publishing \"$PROJECT\" with \"$TAG\" tag"
+    npm publish --dry-run --tag "$TAG"
+  else
+    echo "Publishing \"$PROJECT\" with \"$TAG\" tag"
+    npm publish --tag "$TAG"
+  fi
+done
