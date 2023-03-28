463624957a0be5ace76821db459cfc46a6e639c6
Aleksander Sklorz <Aleksander.Sklorz@hyland.com>
2023-03-28 10:01:34 +0200

    ACS-4126 Fixed e2e

--- e2e/suites/search/search-results-libraries.test.ts
+++ e2e/suites/search/search-results-libraries.test.ts
@@ -187,7 +187,7 @@ describe('Search results - libraries', () => {
     await searchInput.searchFor(site1.name);
     await dataTable.waitForBody();
 
-    const expectedColumns = ['Name', 'Description', 'My Role', 'Visibility', 'Tags'];
+    const expectedColumns = ['Name', 'Description', 'My Role', 'Visibility'];
     const actualColumns = await dataTable.getColumnHeadersText();
 
     await expect(actualColumns).toEqual(expectedColumns);
