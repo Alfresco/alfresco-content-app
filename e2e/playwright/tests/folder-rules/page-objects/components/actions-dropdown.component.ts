0f5c271072b57af2dbbc32818b8454f2a40d97f6
akash.rathod@hyland.com <akash.rathod@hyland.com>
2023-04-13 12:08:51 +0200

    [ACS-4887] e2e for actions variable name fix

--- e2e/playwright/tests/folder-rules/page-objects/components/actions-dropdown.component.ts
+++ e2e/playwright/tests/folder-rules/page-objects/components/actions-dropdown.component.ts
@@ -36,19 +36,19 @@ export enum ActionType {
 export class ActionsDropdownComponent extends BaseComponent {
   private static rootElement = 'aca-rule-action-list';
 
-  public getOptionLocator = (optionName: string): Locator => this.page.locator('.mat-select-panel .mat-option-text', { hasText: optionName });
+  private getOptionLocator = (optionName: string): Locator => this.page.locator('.mat-select-panel .mat-option-text', { hasText: optionName });
   private ruleActionLocator = this.getChild('aca-rule-action [data-automation-id="rule-action-card-view"]');
   private addActionButtonLocator = this.getChild('[data-automation-id="rule-action-list-add-action-button"]');
   private actionDropdownLocator = this.getChild('[data-automation-id="rule-action-select"]');
-  private actionAspectNameLocatorText = '[data-automation-id="header-aspect-name"] mat-select';
-  private actionCheckInInputLocatorText = '[data-automation-id="header-description"] input';
-  private actionAutoDeclareLocatorText = '[data-automation-id="header-version"] mat-select';
-  private actionSimpleWorkflowStepInputLocatorText = '[data-automation-id="header-approve-step"] input';
-  private actionSimpleWorkflowApproveFolderText = `[data-automation-id="header-approve-folder"] input`;
-  private actionSimpleWorkflowActionChoiceText = '[data-automation-id="content-node-selector-actions-choose"]';
-  private actionSimpleWorkflowLabelApproveLocatorText = `[data-automation-id="card-boolean-label-approve-move"]`;
-  private actionSimpleWorkflowSRejectStepLocatorText = '[data-automation-id="header-reject-step"] input';
-  private actionSimpleWorkflowRejectFolderLocatorText = `[data-automation-id="header-reject-folder"] input`;
+  private actionAspectNameLocator = '[data-automation-id="header-aspect-name"] mat-select';
+  private actionCheckInInputLocator = '[data-automation-id="header-description"] input';
+  private actionAutoDeclareLocator = '[data-automation-id="header-version"] mat-select';
+  private actionSimpleWorkflowStepInputLocator = '[data-automation-id="header-approve-step"] input';
+  private actionSimpleWorkflowApproveFolderLocator = `[data-automation-id="header-approve-folder"] input`;
+  private actionSimpleWorkflowActionChoiceLocator = '[data-automation-id="content-node-selector-actions-choose"]';
+  private actionSimpleWorkflowLabelApproveLocator = `[data-automation-id="card-boolean-label-approve-move"]`;
+  private actionSimpleWorkflowSRejectStepLocator = '[data-automation-id="header-reject-step"] input';
+  private actionSimpleWorkflowRejectFolderLocator = `[data-automation-id="header-reject-folder"] input`;
 
   constructor(page: Page) {
     super(page, ActionsDropdownComponent.rootElement);
@@ -70,24 +70,24 @@ export class ActionsDropdownComponent extends BaseComponent {
   }
 
   async insertCheckInActionValues(checkInValue: string, index: number): Promise<void> {
-    await this.ruleActionLocator.nth(index).locator(this.actionCheckInInputLocatorText).type(checkInValue, { delay: 50 });
+    await this.ruleActionLocator.nth(index).locator(this.actionCheckInInputLocator).type(checkInValue, { delay: 50 });
   }
 
   async insertAddAspectActionValues(AspectValue: string, index: number): Promise<void> {
-    await this.dropdownSelection(AspectValue, this.actionAspectNameLocatorText, index);
+    this.dropdownSelection(AspectValue, this.actionAspectNameLocator, index);
   }
 
   async insertAutoDeclareOptionsActionValues(autoDeclareOptionsValue: string, index: number): Promise<void> {
-    await this.dropdownSelection(autoDeclareOptionsValue, this.actionAutoDeclareLocatorText, index);
+    this.dropdownSelection(autoDeclareOptionsValue, this.actionAutoDeclareLocator, index);
   }
 
   async insertSimpleWorkflowActionValues(stepValue: string, index: number): Promise<void> {
-    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowStepInputLocatorText).type(stepValue, { delay: 50 });
-    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowApproveFolderText).click();
-    await this.page.locator(this.actionSimpleWorkflowActionChoiceText).click();
-    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowLabelApproveLocatorText).click();
-    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowSRejectStepLocatorText).type(stepValue, { delay: 50 });
-    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowRejectFolderLocatorText).click();
-    await this.page.locator(this.actionSimpleWorkflowActionChoiceText).click();
+    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowStepInputLocator).type(stepValue, { delay: 50 });
+    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowApproveFolderLocator).click();
+    await this.page.locator(this.actionSimpleWorkflowActionChoiceLocator).click();
+    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowLabelApproveLocator).click();
+    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowSRejectStepLocator).type(stepValue, { delay: 50 });
+    await this.ruleActionLocator.nth(index).locator(this.actionSimpleWorkflowRejectFolderLocator).click();
+    await this.page.locator(this.actionSimpleWorkflowActionChoiceLocator).click();
   }
 }
