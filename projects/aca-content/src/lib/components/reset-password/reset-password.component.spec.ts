3517d48ea927521d667fbc18848922c296d9663d
Sheena Malhotra <sheena.malhotra@contractors.onbase.com>
2023-05-02 01:16:14 +0530

    unit test cleanup

--- projects/aca-content/src/lib/components/reset-password/reset-password.component.spec.ts
+++ projects/aca-content/src/lib/components/reset-password/reset-password.component.spec.ts
@@ -24,28 +24,52 @@
  */
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CoreModule } from '@alfresco/adf-core';
+import { AlfrescoApiService, AlfrescoApiServiceMock, CoreModule } from '@alfresco/adf-core';
 import { ResetPasswordComponent } from './reset-password.component';
 import { AppTestingModule } from '../../testing/app-testing.module';
+import { ActivatedRoute, Router, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
   let fixture: ComponentFixture<ResetPasswordComponent>;
+  const router: any = {
+    url: '',
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AppTestingModule, CoreModule.forChild()],
-      declarations: [ResetPasswordComponent]
+      declarations: [ResetPasswordComponent],
+      providers: [
+        {
+          provide: Router,
+          useValue: router
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { data: { preferencePrefix: 'prefix' }, paramMap: convertToParamMap({ folderId: undefined }) },
+            params: of({ folderId: 'someId' }),
+            queryParamMap: of({})
+          }
+        },
+        {
+          provide: AlfrescoApiService,
+          useClass: AlfrescoApiServiceMock
+        }
+      ]
     });
 
     fixture = TestBed.createComponent(ResetPasswordComponent);
     component = fixture.componentInstance;
-    component.ngOnInit();
+    fixture.detectChanges();
   });
 
-  // afterEach(() => {
-  //   fixture.destroy();
-  // });
+  afterEach(() => {
+    fixture.destroy();
+  });
 
   it('change password button should be disabled by default', async () => {
     expect(component.isSubmitButtonDisabled).toBeTruthy();
@@ -56,9 +80,6 @@ describe('ResetPasswordComponent', () => {
     component.resetPasswordForm.controls['password'].setValue('password');
     component.resetPasswordForm.controls['confirmPassword'].setValue('password');
 
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     expect(component.isSubmitButtonDisabled).toBeTruthy();
   });
 
@@ -66,9 +87,6 @@ describe('ResetPasswordComponent', () => {
     component.resetPasswordForm.controls['username'].setValue('user');
     component.resetPasswordForm.controls['password'].setValue('');
     component.resetPasswordForm.controls['confirmPassword'].setValue('password');
-
-    fixture.detectChanges();
-    await fixture.whenStable();
 
     expect(component.isSubmitButtonDisabled).toBeTruthy();
   });
@@ -78,9 +96,6 @@ describe('ResetPasswordComponent', () => {
     component.resetPasswordForm.controls['password'].setValue('password');
     component.resetPasswordForm.controls['confirmPassword'].setValue('');
 
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     expect(component.isSubmitButtonDisabled).toBeTruthy();
   });
 
@@ -89,16 +104,10 @@ describe('ResetPasswordComponent', () => {
     component.resetPasswordForm.controls['password'].setValue('password1');
     component.resetPasswordForm.controls['confirmPassword'].setValue('password2');
 
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     expect(component.isSubmitButtonDisabled).toBeTruthy();
   });
 
   it('change password button should be enabled when new password and confirm password field values match and username field is not empty', async () => {
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     component.resetPasswordForm.controls['username'].setValue('user');
     component.resetPasswordForm.controls['password'].setValue('password');
     component.resetPasswordForm.controls['confirmPassword'].setValue('password');
@@ -117,14 +126,8 @@ describe('ResetPasswordComponent', () => {
     component.passwordVisibility = false;
     spyOn(component, 'togglePasswordVisibility').and.callThrough();
 
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     const eyeIcon = fixture.debugElement.nativeElement.querySelector('.new-password-visibility-toggle');
     eyeIcon.click();
-
-    fixture.detectChanges();
-    await fixture.whenStable();
 
     expect(component.togglePasswordVisibility).toHaveBeenCalled();
     expect(component.passwordVisibility).toEqual(true);
@@ -134,14 +137,8 @@ describe('ResetPasswordComponent', () => {
     component.confirmPasswordVisibility = false;
     spyOn(component, 'toggleConfirmPasswordVisibility').and.callThrough();
 
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     const eyeIcon = fixture.debugElement.nativeElement.querySelector('.confirm-password-visibility-toggle');
     eyeIcon.click();
-
-    fixture.detectChanges();
-    await fixture.whenStable();
 
     expect(component.toggleConfirmPasswordVisibility).toHaveBeenCalled();
     expect(component.confirmPasswordVisibility).toEqual(true);
@@ -153,14 +150,8 @@ describe('ResetPasswordComponent', () => {
     component.resetPasswordForm.controls['confirmPassword'].setValue('password');
     spyOn(component, 'changePassword').and.callThrough();
 
-    fixture.detectChanges();
-    await fixture.whenStable();
-
     const changePasswordBtn = fixture.debugElement.nativeElement.querySelector('.change-password-button');
     changePasswordBtn.dispatchEvent(new Event('click'));
-
-    fixture.detectChanges();
-    await fixture.whenStable();
 
     expect(component.changePassword).toHaveBeenCalled();
   });
