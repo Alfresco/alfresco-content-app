bb040dc47013da73b45a650720be6c9714fd96bd
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-05-03 05:08:24 +0000

    fix for e2es

--- projects/aca-testing-shared/src/components/pageLayoutHeader/pageLayoutHeader.ts
+++ projects/aca-testing-shared/src/components/pageLayoutHeader/pageLayoutHeader.ts
@@ -33,7 +33,7 @@ import { BrowserActions } from '@alfresco/adf-testing';
 
 export class PageLayoutHeader extends Component {
   logoLink = this.byCss('.app-menu__title');
-  moreActions = browser.element(by.id('app.header.more'));
+  moreActions = this.byCss('.aca-menu-user-details-button');
   sidenavToggle = this.byCss(`.sidenav-header-title-logo`);
 
   menu = new Menu();
