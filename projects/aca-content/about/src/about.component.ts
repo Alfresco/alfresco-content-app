822467827321ab616824f1af783e869191e5767a
Denys Vuika <denys.vuika@gmail.com>
2023-05-24 11:21:10 +0100

    experimental: standalone About component

--- projects/aca-content/about/src/about.component.ts
+++ projects/aca-content/about/src/about.component.ts
@@ -22,46 +22,37 @@
  * from Hyland Software. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { DEV_MODE_TOKEN } from './dev-mode.tokens';
-import { Observable } from 'rxjs';
-import { AppExtensionService, ExtensionRef } from '@alfresco/adf-extensions';
-import { AppConfigService, AuthenticationService, RepositoryInfo } from '@alfresco/adf-core';
+import { AppExtensionService } from '@alfresco/adf-extensions';
+import { AboutModule, AppConfigService, AuthenticationService, RepositoryInfo } from '@alfresco/adf-core';
 import { DiscoveryApiService } from '@alfresco/adf-content-services';
 import { PACKAGE_JSON } from './package-json.token';
+import { TranslateModule } from '@ngx-translate/core';
+import { PageLayoutModule } from '@alfresco/aca-shared';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule, TranslateModule, AboutModule, PageLayoutModule, RouterModule, MatIconModule, MatButtonModule],
   selector: 'app-about-page',
   templateUrl: './about.component.html',
-  styles: [
-    `
-      adf-about {
-        width: 100%;
-      }
-    `
-  ]
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  pkg: any;
-  dev = false;
-  extensions$: Observable<ExtensionRef[]>;
-  repository: RepositoryInfo = null;
-  landingPage: string;
+  private authService = inject(AuthenticationService);
+  private appExtensions = inject(AppExtensionService);
+  private discovery = inject(DiscoveryApiService);
+  private appConfigService = inject(AppConfigService);
+  public packageJson? = inject(PACKAGE_JSON, { optional: true });
+  public dev = inject(DEV_MODE_TOKEN);
 
-  constructor(
-    @Inject(DEV_MODE_TOKEN) devMode,
-    @Optional()
-    @Inject(PACKAGE_JSON)
-    public packageJson,
-    private authService: AuthenticationService,
-    private appExtensions: AppExtensionService,
-    private discovery: DiscoveryApiService,
-    appConfigService: AppConfigService
-  ) {
-    this.dev = !devMode;
-    this.extensions$ = this.appExtensions.references$;
-    this.landingPage = appConfigService.get('landingPage', '/personal-files');
-  }
+  extensions$ = this.appExtensions.references$;
+  repository: RepositoryInfo = null;
+  landingPage = this.appConfigService.get('landingPage', '/personal-files');
 
   ngOnInit(): void {
     if (this.authService.isEcmLoggedIn()) {
