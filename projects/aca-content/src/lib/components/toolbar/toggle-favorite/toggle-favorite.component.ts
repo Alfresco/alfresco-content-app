b39f55b30ec158af683c6ec2952bf0d7da04974f
swapnil.verma <swapnil.verma@globallogic.com>
2023-06-02 15:53:00 +0530

    [ACS-5088] Resolved typo in ToggleFavoriteComponent

--- projects/aca-content/src/lib/components/toolbar/toggle-favorite/toggle-favorite.component.ts
+++ projects/aca-content/src/lib/components/toolbar/toggle-favorite/toggle-favorite.component.ts
@@ -33,9 +33,9 @@ import { Router } from '@angular/router';
   selector: 'app-toggle-favorite',
   template: `
     <button mat-menu-item #favorites="adfFavorite" (toggle)="onToggleEvent()" [adf-node-favorite]="(selection$ | async).nodes">
-      <mat-icon *ngIf="favorites.hasFavorites">star</mat-icon>
-      <mat-icon *ngIf="!favorites.hasFavorites">star_border</mat-icon>
-      <span>{{ (favorites.hasFavorites ? 'APP.ACTIONS.REMOVE_FAVORITE' : 'APP.ACTIONS.FAVORITE') | translate }}</span>
+      <mat-icon *ngIf="favorites.hasFavorites()">star</mat-icon>
+      <mat-icon *ngIf="!favorites.hasFavorites()">star_border</mat-icon>
+      <span>{{ (favorites.hasFavorites() ? 'APP.ACTIONS.REMOVE_FAVORITE' : 'APP.ACTIONS.FAVORITE') | translate }}</span>
     </button>
   `,
   encapsulation: ViewEncapsulation.None,
