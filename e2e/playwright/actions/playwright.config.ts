f810d3e7b269541ee988954edcc0507584bd2331
akash.rathod@hyland.com <akash.rathod@hyland.com>
2023-06-14 12:16:35 +0200

    changes for git

--- e2e/playwright/actions/playwright.config.ts
+++ e2e/playwright/actions/playwright.config.ts
@@ -35,7 +35,7 @@ const config: PlaywrightTestConfig<CustomConfig> = {
       name: 'Actions',
       testDir: './src/tests',
       use: {
-        users: ['hruser']
+        users: ['contentIdentity']
       }
     }
   ]
--- e2e/playwright/actions/src/tests/create-folder.spec.ts
+++ e2e/playwright/actions/src/tests/create-folder.spec.ts
@@ -25,7 +25,7 @@
 import { expect } from '@playwright/test';
 import { getUserState, test } from '@alfresco/playwright-shared';
 
-test.use({ storageState: getUserState('admin') });
+test.use({ storageState: getUserState('contentIdentity') });
 test.describe('Create actions', () => {
   let randomFolderName: string;
 
