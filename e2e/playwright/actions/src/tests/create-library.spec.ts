f9bc90abc8ee9719f2ceaf890d7493316207bbbb
adam.zakrzewski <adam.zakrzewski@hyland.com>
2023-07-07 19:40:18 +0200

    updating tests and adding before and after all

--- e2e/playwright/actions/src/tests/create-library.spec.ts
+++ e2e/playwright/actions/src/tests/create-library.spec.ts
@@ -23,11 +23,13 @@
  */
 
 import { expect } from '@playwright/test';
-import { getUserState, test } from '@alfresco/playwright-shared';
+import { ApiClientFactory, getUserState, test } from '@alfresco/playwright-shared';
 import { libraryErrors } from '../../../../../projects/aca-playwright-shared/src/utils/library-errors';
+import { SiteBodyCreate } from '@alfresco/js-api';
 
 test.use({ storageState: getUserState('hruser') });
 test.describe('Create Libraries ', () => {
+  const apiClientFactory = new ApiClientFactory();
   let randomLibraryName: string;
   let randomLibraryId: string;
   let randomLibraryDescription: string;
@@ -40,11 +42,31 @@ test.describe('Create Libraries ', () => {
   const privateVisibility = 'Private';
   const deleteAction = 'Delete';
 
+  const commonLibraryName = `playwright-library-${(Math.random() + 1).toString(36).substring(6)}`;
+  const commonLibraryId = `libraryId-${(Math.random() + 1).toString(36).substring(6)}`;
+  const commonTrashLibraryName = `playwright-library-${(Math.random() + 1).toString(36).substring(6)}`;
+  const commonTrashLibraryId = `libraryId-${(Math.random() + 1).toString(36).substring(6)}`;
+
+  test.beforeAll(async () => {
+    await apiClientFactory.setUpAcaBackend('hruser');
+    await apiClientFactory.sites.createSite({ id: commonLibraryId, title: commonLibraryName, visibility: SiteBodyCreate.VisibilityEnum.PUBLIC });
+    await apiClientFactory.sites.createSite({
+      id: commonTrashLibraryId,
+      title: commonTrashLibraryName,
+      visibility: SiteBodyCreate.VisibilityEnum.PUBLIC
+    });
+    await apiClientFactory.sites.deleteSite(commonTrashLibraryId);
+  });
+
   test.beforeEach(async ({ myLibrariesPage }) => {
     randomLibraryName = `playwright-library-${(Math.random() + 1).toString(36).substring(6)}`;
     randomLibraryId = `libraryId-${(Math.random() + 1).toString(36).substring(6)}`;
     randomLibraryDescription = `libraryDescription-${(Math.random() + 1).toString(36).substring(6)}`;
     await myLibrariesPage.navigate();
+  });
+
+  test.afterAll(async () => {
+    await apiClientFactory.sites.deleteSite(commonLibraryId, { permanent: true });
   });
 
   test('[C280024] Create Library dialog UI', async ({ myLibrariesPage }) => {
@@ -70,7 +92,6 @@ test.describe('Create Libraries ', () => {
     await expect(myLibrariesPage.libraryDialog.getLabelText(libraryNameLebel)).toHaveValue(randomLibraryName);
     await expect(myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel)).toHaveValue(randomLibraryName);
     await myLibrariesPage.libraryDialog.createButton.click();
-
     await expect(myLibrariesPage.breadcrumb.getBreadcrumbItem(randomLibraryName)).toBeVisible();
 
     await myLibrariesPage.navigate();
@@ -110,7 +131,6 @@ test.describe('Create Libraries ', () => {
     await myLibrariesPage.acaHeader.createButton.click();
     await myLibrariesPage.matMenu.createLibrary.click();
     await myLibrariesPage.libraryDialog.createLibraryWithNameAndId(randomLibraryName, randomLibraryId, randomLibraryDescription);
-
     await expect(myLibrariesPage.breadcrumb.getBreadcrumbItem(randomLibraryName)).toBeVisible();
 
     await myLibrariesPage.navigate();
@@ -118,7 +138,6 @@ test.describe('Create Libraries ', () => {
     await expect(
       myLibrariesPage.dataTable.getCellLinkByName(randomLibraryName).and(myLibrariesPage.page.getByTitle(randomLibraryDescription))
     ).toBeVisible();
-
     await myLibrariesPage.dataTable.getRowByName(randomLibraryName).click();
     await myLibrariesPage.acaHeader.viewDetails.click();
     await expect(myLibrariesPage.viewDetails.getFieldData('Name').and(myLibrariesPage.viewDetails.getFieldData(randomLibraryName))).toBeVisible();
@@ -130,49 +149,31 @@ test.describe('Create Libraries ', () => {
       myLibrariesPage.viewDetails.getFieldData(libraryDescriptionLebel).and(myLibrariesPage.viewDetails.getFieldData(randomLibraryDescription))
     ).toBeVisible();
 
-    await myLibrariesPage.dataTable.performActionFromExpandableMenu(randomLibraryName, deleteAction);
+    await apiClientFactory.sites.deleteSite(randomLibraryId, { permanent: true });
   });
 
   test('[C280027] Duplicate library ID', async ({ myLibrariesPage }) => {
     await myLibrariesPage.acaHeader.createButton.click();
     await myLibrariesPage.matMenu.createLibrary.click();
-    await myLibrariesPage.libraryDialog.createLibraryWithNameAndId(randomLibraryName, randomLibraryId);
-
-    await myLibrariesPage.navigate();
-    // duplicate library id
-    await myLibrariesPage.acaHeader.createButton.click();
-    await myLibrariesPage.matMenu.createLibrary.click();
-    await myLibrariesPage.libraryDialog.getLabelText(libraryNameLebel).fill(randomLibraryName + '2');
+    await myLibrariesPage.libraryDialog.getLabelText(libraryNameLebel).fill(randomLibraryName);
     await myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel).clear();
-    await myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel).fill(randomLibraryId);
+    await myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel).fill(commonLibraryId);
 
     await expect(myLibrariesPage.libraryDialog.createButton).toBeDisabled();
     await expect(myLibrariesPage.libraryDialog.getMatError(libraryErrors.libraryIdIsNotAvailable)).toBeVisible();
-
-    await myLibrariesPage.libraryDialog.cancelButton.click();
-    await myLibrariesPage.dataTable.performActionFromExpandableMenu(randomLibraryName, deleteAction);
   });
 
   test('[C280028] Create library using the ID of a library from the Trashcan', async ({ myLibrariesPage }) => {
     await myLibrariesPage.acaHeader.createButton.click();
     await myLibrariesPage.matMenu.createLibrary.click();
-    await myLibrariesPage.libraryDialog.createLibraryWithNameAndId(randomLibraryName, randomLibraryId);
-
-    await myLibrariesPage.navigate();
-    await myLibrariesPage.dataTable.performActionFromExpandableMenu(randomLibraryName, deleteAction);
-    // library id from the trashcan
-    await myLibrariesPage.acaHeader.createButton.click();
-    await myLibrariesPage.matMenu.createLibrary.click();
-    await myLibrariesPage.libraryDialog.getLabelText(libraryNameLebel).fill(randomLibraryName + '2');
+    await myLibrariesPage.libraryDialog.getLabelText(libraryNameLebel).fill(randomLibraryName);
     await myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel).clear();
-    await myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel).fill(randomLibraryId);
+    await myLibrariesPage.libraryDialog.getLabelText(libraryIdLebel).fill(commonTrashLibraryId);
 
     await expect(myLibrariesPage.libraryDialog.createButton).toBeEnabled();
     await myLibrariesPage.libraryDialog.createButton.click();
     await expect(myLibrariesPage.libraryDialog.createButton).toBeDisabled();
     await expect(myLibrariesPage.libraryDialog.getMatError(libraryErrors.libraryIdIsAlreadyUsed)).toBeVisible();
-
-    await myLibrariesPage.libraryDialog.cancelButton.click();
   });
 
   test('[C280029] Cancel button', async ({ myLibrariesPage }) => {
@@ -218,25 +219,17 @@ test.describe('Create Libraries ', () => {
       await expect(myLibrariesPage.libraryDialog.getMatError(libraryErrors.useNumbersAndLettersOnly)).toBeVisible();
       await expect(myLibrariesPage.libraryDialog.createButton).toBeDisabled();
     }
-
-    await myLibrariesPage.libraryDialog.cancelButton.click();
   });
 
   test('[C280030] Create 2 libraries with same name but different IDs', async ({ myLibrariesPage }) => {
-    const libraryName = randomLibraryName + ' (' + randomLibraryId + ')';
-    const libraryName2 = randomLibraryName + ' (' + randomLibraryId + '2)';
+    const libraryName = commonLibraryName + ' (' + commonLibraryId + ')';
+    const libraryName2 = commonLibraryName + ' (' + randomLibraryId + ')';
 
     await myLibrariesPage.acaHeader.createButton.click();
     await myLibrariesPage.matMenu.createLibrary.click();
-    await myLibrariesPage.libraryDialog.createLibraryWithNameAndId(randomLibraryName, randomLibraryId);
-    await myLibrariesPage.navigate();
+    await myLibrariesPage.libraryDialog.createLibraryWithNameAndId(commonLibraryName, randomLibraryId);
 
-    // duplicate library name but different id
-    await myLibrariesPage.acaHeader.createButton.click();
-    await myLibrariesPage.matMenu.createLibrary.click();
-    await myLibrariesPage.libraryDialog.createLibraryWithNameAndId(randomLibraryName, randomLibraryId + '2');
-
-    await expect(myLibrariesPage.breadcrumb.getBreadcrumbItem(randomLibraryName)).toBeVisible();
+    await expect(myLibrariesPage.breadcrumb.getBreadcrumbItem(commonLibraryName)).toBeVisible();
 
     await myLibrariesPage.navigate();
     await myLibrariesPage.dataTable.goThroughPagesLookingForRowWithName(libraryName);
@@ -244,7 +237,6 @@ test.describe('Create Libraries ', () => {
     await myLibrariesPage.dataTable.goThroughPagesLookingForRowWithName(libraryName2);
     await expect(myLibrariesPage.dataTable.getRowByName(libraryName2)).toBeVisible();
 
-    await myLibrariesPage.dataTable.performActionFromExpandableMenu(libraryName2, deleteAction);
-    await myLibrariesPage.dataTable.performActionFromExpandableMenu(randomLibraryName, deleteAction);
+    await apiClientFactory.sites.deleteSite(randomLibraryId, { permanent: true });
   });
 });
