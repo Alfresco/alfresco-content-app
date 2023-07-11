b192c5f6bbc49a9d6306a239f8da403659d69000
Denys Vuika <denys.vuika@gmail.com>
2023-07-11 12:25:55 +0100

    [ACA-4715] cleanup Docker and update documentation (#3328)
    * move ADF linking to the dev docs
    
    * add basic Docker docs
    
    * reduce docker params
    
    * update docker docs

--- docker/run.sh
+++ docker/run.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
-
-eval ALL_ARGUMENTS=("BASE_PATH"
-)
-ALL_ARGUMENTS_LENGTH=${#ALL_ARGUMENTS[@]}
-DOCKER_ARGS=""
-for (( j=0; j<${ALL_ARGUMENTS_LENGTH}; j++ )); do
-    ARG="${ALL_ARGUMENTS[$j]}"
-    if [  -v "${ARG}" ]; then
-        DOCKER_ARGS="$DOCKER_ARGS --env $ARG=${!ARG}"
-    fi
-done
-
+DOCKER_IMAGE_REPO=alfresco/alfresco-content-app
 HOST_PORT=8081
 CONTAINER_PORT=8080
+
 docker rmi -f $DOCKER_IMAGE_REPO
-docker build --build-arg PROJECT_NAME=$PROJECT_NAME -t $DOCKER_IMAGE_REPO .
-echo "http://localhost:${HOST_PORT}${BASE_PATH}"
-docker run --rm -it $DOCKER_ARGS --user 1000:1000 --publish $HOST_PORT:$CONTAINER_PORT $DOCKER_IMAGE_REPO
+docker build -t $DOCKER_IMAGE_REPO .
+
+echo "http://localhost:${HOST_PORT}"
+docker run --rm -it --user 1000:1000 --publish $HOST_PORT:$CONTAINER_PORT $DOCKER_IMAGE_REPO
