86c19af0a7a39bdbb2909a9d966b53c89a9b7b7d
Denys Vuika <denys.vuika@gmail.com>
2023-07-11 11:19:38 +0100

    reduce docker params

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME=content-ce
 DOCKER_IMAGE_REPO=alfresco/alfresco-content-app
 HOST_PORT=8081
 CONTAINER_PORT=8080
 
 docker rmi -f $DOCKER_IMAGE_REPO
-docker build --build-arg PROJECT_NAME=$PROJECT_NAME -t $DOCKER_IMAGE_REPO .
+docker build -t $DOCKER_IMAGE_REPO .
 
 echo "http://localhost:${HOST_PORT}"
 docker run --rm -it --user 1000:1000 --publish $HOST_PORT:$CONTAINER_PORT $DOCKER_IMAGE_REPO
