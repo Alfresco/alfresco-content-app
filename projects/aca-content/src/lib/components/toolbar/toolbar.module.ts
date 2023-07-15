5e5f4a227aa9a84ec4df64c264a2f65fe7e9a0f0
Denys Vuika <denys.vuika@gmail.com>
2023-07-15 17:54:12 +0100

    view node

--- projects/aca-content/src/lib/components/toolbar/toolbar.module.ts
+++ projects/aca-content/src/lib/components/toolbar/toolbar.module.ts
@@ -25,12 +25,8 @@
 import { NgModule } from '@angular/core';
 import { ToggleFavoriteComponent } from './toggle-favorite/toggle-favorite.component';
 import { ToggleInfoDrawerComponent } from './toggle-info-drawer/toggle-info-drawer.component';
-import { CommonModule } from '@angular/common';
-import { CoreModule } from '@alfresco/adf-core';
-import { ExtensionsModule } from '@alfresco/adf-extensions';
 import { ToggleJoinLibraryButtonComponent } from './toggle-join-library/toggle-join-library-button.component';
 import { ToggleJoinLibraryMenuComponent } from './toggle-join-library/toggle-join-library-menu.component';
-import { DirectivesModule } from '../../directives/directives.module';
 import { ToggleFavoriteLibraryComponent } from './toggle-favorite-library/toggle-favorite-library.component';
 import { ToggleEditOfflineComponent } from './toggle-edit-offline/toggle-edit-offline.component';
 import { ViewNodeComponent } from './view-node/view-node.component';
@@ -38,19 +34,14 @@ import { SharedToolbarModule } from '@alfresco/aca-shared';
 
 @NgModule({
   imports: [
-    CommonModule,
-    CoreModule.forChild(),
-    ExtensionsModule,
-    SharedToolbarModule,
-    DirectivesModule,
     ToggleFavoriteComponent,
     ToggleEditOfflineComponent,
     ToggleInfoDrawerComponent,
     ToggleJoinLibraryButtonComponent,
     ToggleJoinLibraryMenuComponent,
-    ToggleFavoriteLibraryComponent
+    ToggleFavoriteLibraryComponent,
+    ViewNodeComponent
   ],
-  declarations: [ViewNodeComponent],
   exports: [
     ToggleFavoriteComponent,
     ToggleInfoDrawerComponent,
