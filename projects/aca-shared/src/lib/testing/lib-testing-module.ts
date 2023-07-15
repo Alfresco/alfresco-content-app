5aa6a91d6de172d619076668fcf1eb240f154200
Denys Vuika <denys.vuika@gmail.com>
2023-07-15 23:46:14 +0100

    remove incorrect import

--- projects/aca-shared/src/lib/testing/lib-testing-module.ts
+++ projects/aca-shared/src/lib/testing/lib-testing-module.ts
@@ -93,7 +93,6 @@ export const initialState = {
       }
     ),
     EffectsModule.forRoot([]),
-    EffectsModule.forRoot([]),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
