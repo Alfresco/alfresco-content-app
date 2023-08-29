95c44b1c312ee871e04ceb5aeceef57a17d84299
DominikIwanek <dominik.iwanek@hyland.com>
2023-08-29 06:24:00 +0200

    Change e2e and unit tests

--- projects/aca-content/src/lib/components/toolbar/view-node/view-node.component.spec.ts
+++ projects/aca-content/src/lib/components/toolbar/view-node/view-node.component.spec.ts
@@ -118,7 +118,7 @@ describe('ViewNodeComponent', () => {
       iconButton: true
     };
     mockStore.select.and.returnValue(of(linkNode));
-
+    route.queryParams = of({});
     fixture.detectChanges();
 
     component.onClick();
