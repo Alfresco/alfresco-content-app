7e9b49795101bff8c05dcabc6cd9ad29907d7679
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-09-01 15:09:42 +0530

    [ACS-5540]fixed unit test cases and added unit test cases

--- projects/aca-content/src/lib/components/details/details.component.spec.ts
+++ projects/aca-content/src/lib/components/details/details.component.spec.ts
@@ -43,6 +43,7 @@ describe('DetailsComponent', () => {
   let contentApiService: ContentApiService;
   let store: Store;
   let node: NodeEntry;
+  let mockNodeAspectService: jasmine.SpyObj<NodeAspectService>;
 
   const mockStream = new Subject();
   const storeMock = {
@@ -92,6 +93,7 @@ describe('DetailsComponent', () => {
   };
 
   beforeEach(() => {
+    const nodeAspectServiceSpy = jasmine.createSpyObj('NodeAspectService', ['updateNodeAspects']);
     TestBed.configureTestingModule({
       imports: [AppTestingModule, DetailsComponent],
       providers: [
@@ -120,6 +122,10 @@ describe('DetailsComponent', () => {
             onLogout: new Subject<any>(),
             isLoggedIn: () => true
           }
+        },
+        {
+          provide: NodeAspectService,
+          useValue: nodeAspectServiceSpy
         }
       ],
       schemas: [NO_ERRORS_SCHEMA]
@@ -128,6 +134,8 @@ describe('DetailsComponent', () => {
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
     contentApiService = TestBed.inject(ContentApiService);
+    mockNodeAspectService = TestBed.inject(NodeAspectService) as jasmine.SpyObj<NodeAspectService>;
+    component.node = { id: 'test-id' } as MinimalNodeEntryEntity;
     store = TestBed.inject(Store);
 
     node = {
