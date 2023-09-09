7866c4017c89d53fe03645e337d6a0c268b2c1d5
pkundu <priyanka.kundu@hyland.com>
2023-09-09 12:43:04 +0530

    [ACS-5551]added null checks

--- projects/aca-shared/src/lib/components/info-drawer/info-drawer.component.spec.ts
+++ projects/aca-shared/src/lib/components/info-drawer/info-drawer.component.spec.ts
@@ -57,6 +57,38 @@ describe('InfoDrawerComponent', () => {
       ])
   };
 
+  const mockNode = {
+    isFile: false,
+    createdByUser: { id: 'admin', displayName: 'Administrator' },
+    modifiedAt: new Date('2017-05-24T15:08:55.640Z'),
+    nodeType: 'cm:content',
+    content: {
+      mimeType: 'application/rtf',
+      mimeTypeName: 'Rich Text Format',
+      sizeInBytes: 14530,
+      encoding: 'UTF-8'
+    },
+    parentId: 'd124de26-6ba0-4f40-8d98-4907da2d337a',
+    createdAt: new Date('2017-05-24T15:08:55.640Z'),
+    path: {
+      name: '/Company Home/Guest Home',
+      isComplete: true,
+      elements: [
+        {
+          id: '94acfc73-7014-4475-9bd9-93a2162f0f8c',
+          name: 'Company Home'
+        },
+        { id: 'd124de26-6ba0-4f40-8d98-4907da2d337a', name: 'Guest Home' }
+      ]
+    },
+    isFolder: true,
+    modifiedByUser: { id: 'admin', displayName: 'Administrator' },
+    name: 'b_txt_file.rtf',
+    id: '70e1cc6a-6918-468a-b84a-1048093b06fd',
+    properties: { 'cm:versionLabel': '1.0', 'cm:versionType': 'MAJOR' },
+    allowableOperations: ['delete', 'update']
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [LibTestingModule, InfoDrawerComponent],
@@ -181,5 +213,13 @@ describe('InfoDrawerComponent', () => {
         icon: 'highlight_off'
       } as ContentActionRef
     ]);
+  });
+
+  it('should return the icon when getNodeIcon is called', () => {
+    const expectedIcon = 'assets/images/ft_ic_folder';
+    spyOn(component['nodeActionsService'], 'getNodeIcon').and.returnValue(expectedIcon);
+    fixture.detectChanges();
+    const result = component.getNodeIcon(mockNode);
+    expect(result).toContain(expectedIcon);
   });
 });
