dc0497289bd7c9410c89e101806e67885cc06bc0
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-09-22 20:56:13 +0530

    [ACS-5540] added group lock changes

--- projects/aca-content/src/lib/components/info-drawer/metadata-tab/metadata-tab.component.ts
+++ projects/aca-content/src/lib/components/info-drawer/metadata-tab/metadata-tab.component.ts
@@ -50,6 +50,7 @@ import { Actions, ofType } from '@ngrx/effects';
       [(editable)]="editable"
       [(editableTags)]="editableTags"
       [(editableCategories)]="editableCategories"
+      [(group)]="group"
     >
     </adf-content-metadata>
   `,
@@ -67,6 +68,9 @@ export class MetadataTabComponent implements OnInit, OnDestroy {
   customPanels: Observable<ContentMetadataCustomPanel[]>;
   editableTags = false;
   editableCategories = false;
+  group: any = {
+    editable: false
+  };
 
   constructor(
     private permission: NodePermissionService,
@@ -100,6 +104,7 @@ export class MetadataTabComponent implements OnInit, OnDestroy {
           this.editable = false;
           this.editableTags = false;
           this.editableCategories = false;
+          this.group.editable = false;
         }
       });
     this.customPanels = this.extensions.getCustomMetadataPanels({ entry: this.node }).pipe(
