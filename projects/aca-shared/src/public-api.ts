bcd3b22ddbdf94c5c391631952eb120fd59ad917
MichalKinas <michal.kinas@hyland.com>
2023-09-26 14:44:43 +0200

    [ACS-5601] Restore modal configuration, prettier fixes

--- projects/aca-shared/src/public-api.ts
+++ projects/aca-shared/src/public-api.ts
@@ -45,6 +45,7 @@ export * from './lib/directives/pagination.directive';
 
 export * from './lib/models/types';
 export * from './lib/models/viewer.rules';
+export * from './lib/models/modal-configuration';
 
 export * from './lib/routing/shared.guard';
 
