4d86d0c861e64417ac69e54c089fe935e0cfc876
Anukriti Singh <anukriti.singh@hyland.com>
2023-10-12 11:03:25 +0530

    [ACS-5551] update style

--- projects/aca-content/src/lib/components/details/details.component.spec.ts
+++ projects/aca-content/src/lib/components/details/details.component.spec.ts
@@ -188,7 +188,9 @@ describe('DetailsComponent', () => {
     fixture.whenStable().then(() => {
       expect(component.aspectActions).toEqual(mockAspectActions);
     });
-  it('should return the icon when getInfoDrawerIcon is called', () => {
+  });
+
+  it('should return the icon when getNodeIcon is called', () => {
     const expectedIcon = 'assets/images/ft_ic_folder';
     spyOn(component['nodeActionsService'], 'getNodeIcon').and.returnValue(expectedIcon);
     fixture.detectChanges();
