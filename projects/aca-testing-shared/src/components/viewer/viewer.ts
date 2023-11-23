d95f1979def19aef8665e7f8d98730e5cdd32944
Anukriti Singh <anukriti.singh@hyland.com>
2023-11-23 12:13:14 +0530

    [MNT-23433] e2e fix for protractor

--- projects/aca-testing-shared/src/components/viewer/viewer.ts
+++ projects/aca-testing-shared/src/components/viewer/viewer.ts
@@ -32,7 +32,7 @@ export class Viewer extends Component {
   root = browser.$('adf-viewer');
   viewerLayout = this.byCss('.adf-viewer-render-layout-content');
   viewerContainer = this.byCss('.adf-viewer-render-content-container');
-  closeButton = this.byCss('button[id="app.viewer.close"]');
+  closeButton = this.byCss('button[title="Close"]');
   fileTitle = this.byCss('.adf-viewer__file-title');
   viewerExtensionContent = this.byCss('adf-preview-extension');
   txtViewerContent = this.byCss('.adf-txt-viewer-content');
