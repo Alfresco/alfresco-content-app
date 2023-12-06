34c8976af6c962409efaadcc7cf7c7410249ca32
swapnil-verma-gl <92505353+swapnil-verma-gl@users.noreply.github.com>
2023-12-06 13:36:00 +0530

    [ACS-6211] Resolved UI alignment issue in create rule dialog (#3505)
    * [ACS-6211] Resolved UI alignment issue in create rule dialog
    
    * [ACS-6211] Changed appearance of mat-select form field to standard to match with other action form fields
    
    * [ACS-6211] Fixed unit tests
    
    * [ACS-6138] Updating ADW Styling after ACA changes for angular material - Expansion panel items
    
    * [ACS-6211] Fixed failing E2E

--- projects/aca-playwright-shared/src/page-objects/components/actions-dropdown.component.ts
+++ projects/aca-playwright-shared/src/page-objects/components/actions-dropdown.component.ts
@@ -53,7 +53,7 @@ export class ActionsDropdownComponent extends BaseComponent {
   private static rootElement = 'aca-rule-action-list';
 
   private getOptionLocator = (optionName: string): Locator => this.page.locator('.mat-select-panel .mat-option-text', { hasText: optionName }).first();
-  private ruleActionLocator = this.getChild('aca-rule-action [data-automation-id="rule-action-card-view"]');
+  private ruleActionLocator = this.getChild('aca-rule-action');
   private addActionButtonLocator = this.getChild('[data-automation-id="rule-action-list-add-action-button"]');
   private actionDropdownLocator = this.getChild('[data-automation-id="rule-action-select"]');
   private actionAspectNameLocator = '[data-automation-id="header-aspect-name"] mat-select';
