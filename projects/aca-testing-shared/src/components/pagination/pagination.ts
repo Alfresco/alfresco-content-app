c93174cdc0a11f046d44df2c52c36e0e2560f286
Denys Vuika <denys.vuika@gmail.com>
2024-01-09 17:37:52 -0500

    cleanup

--- projects/aca-testing-shared/src/components/pagination/pagination.ts
+++ projects/aca-testing-shared/src/components/pagination/pagination.ts
@@ -22,8 +22,6 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { browser } from 'protractor';
-import { BrowserActions, Logger } from '@alfresco/adf-testing';
 import { Menu } from '../menu/menu';
 import { Component } from '../component';
 
@@ -34,71 +32,11 @@ export class Pagination extends Component {
   totalPages = this.byCss('.adf-pagination__total-pages');
   previousButton = this.byCss('.adf-pagination__previous-button');
   nextButton = this.byCss('.adf-pagination__next-button');
-  maxItemsButton = this.byCss('.adf-pagination__max-items + button[mat-icon-button]');
-  pagesButton = this.byCss('.adf-pagination__current-page + button[mat-icon-button]');
 
   menu: Menu = new Menu();
 
   constructor(ancestor?: string) {
     super('adf-pagination', ancestor);
-  }
-
-  async openMaxItemsMenu() {
-    try {
-      await BrowserActions.click(this.maxItemsButton);
-      await this.menu.waitForMenuToOpen();
-    } catch (error) {
-      Logger.error('____ open max items catch ___', error);
-    }
-  }
-
-  async openCurrentPageMenu() {
-    try {
-      await BrowserActions.click(this.pagesButton);
-      await this.menu.waitForMenuToOpen();
-    } catch (error) {
-      Logger.error('____ open current page menu ___', error);
-    }
-  }
-
-  async resetToDefaultPageSize() {
-    try {
-      await this.openMaxItemsMenu();
-      await this.menu.clickNthItem(1);
-      await this.menu.waitForMenuToClose();
-    } catch (error) {
-      Logger.error('___ reset to default page size catch ___', error);
-    }
-  }
-
-  async resetToDefaultPageNumber() {
-    try {
-      await this.openCurrentPageMenu();
-      await this.menu.clickNthItem(1);
-      await this.menu.waitForMenuToClose();
-    } catch (error) {
-      Logger.error('____ reset to default page number catch ___', error);
-    }
-  }
-
-  async clickNext() {
-    await BrowserActions.click(this.nextButton);
-  }
-
-  async clickPrevious() {
-    await BrowserActions.click(this.previousButton);
-  }
-
-  async isNextEnabled() {
-    return this.nextButton.isEnabled();
-  }
-
-  async isPreviousEnabled() {
-    return this.previousButton.isEnabled();
-  }
-
-  async isPagesButtonPresent() {
-    return browser.isElementPresent(this.pagesButton);
   }
 
   async isRangePresent() {
@@ -125,19 +63,7 @@ export class Pagination extends Component {
     return this.nextButton.isPresent();
   }
 
-  async getCurrentPage() {
-    return this.currentPage.getText();
-  }
-
   async getRange() {
     return this.range.getText();
-  }
-
-  async getMaxItems() {
-    return this.maxItems.getText();
-  }
-
-  async getTotalPages() {
-    return this.totalPages.getText();
   }
 }
