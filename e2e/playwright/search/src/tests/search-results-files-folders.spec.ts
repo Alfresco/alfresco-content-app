e77b4ec8fefaa475960db80d10fa3c2af7c0918e
Katarzyna Kita <katarzyna.kita@hyland.com>
2024-03-11 14:58:23 +0100

    Fixed method name

--- e2e/playwright/search/src/tests/search-results-files-folders.spec.ts
+++ e2e/playwright/search/src/tests/search-results-files-folders.spec.ts
@@ -56,7 +56,7 @@ test.describe('Search results - files and folders', () => {
     const fileRussian = `любимый-сайт-${random}`;
 
     await nodesApi.createFile(fileRussian);
-    await searchPage.searchWithString(fileRussian);
+    await searchPage.searchFilesAndFolders(fileRussian);
     await expect(searchPage.dataTable.getRowByName(fileRussian)).toBeVisible();
   });
 
@@ -66,7 +66,7 @@ test.describe('Search results - files and folders', () => {
     const fileDescription = 'file description';
 
     await nodesApi.createFile(file, '-my-', fileTitle, fileDescription);
-    await searchPage.searchWithString(file);
+    await searchPage.searchFilesAndFolders(file);
     await searchPage.dataTable.clickItemLocation(file);
     expect((await personalFiles.breadcrumb.items.textContent()).trim()).toEqual('Personal Files');
   });
