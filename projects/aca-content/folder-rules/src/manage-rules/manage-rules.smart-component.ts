b4dc7be2ad1ea683811729b31fa56e0f2eb70e28
Mykyta Maliarchuk <84377976+nikita-web-ua@users.noreply.github.com>
2024-03-28 13:01:05 +0100

    [ACS-7366] fix infinite re-rendering (#3739)

--- projects/aca-content/folder-rules/src/manage-rules/manage-rules.smart-component.ts
+++ projects/aca-content/folder-rules/src/manage-rules/manage-rules.smart-component.ts
@@ -211,7 +211,8 @@ export class ManageRulesSmartComponent implements OnInit, OnDestroy {
   }
 
   async onRuleEnabledToggle(rule: Rule, isEnabled: boolean) {
-    await this.folderRulesService.updateRule(this.nodeId, rule.id, { ...rule, isEnabled });
+    const updatedRule = await this.folderRulesService.updateRule(this.nodeId, rule.id, { ...rule, isEnabled });
+    this.folderRuleSetsService.addOrUpdateRuleInMainRuleSet(updatedRule);
   }
 
   async onInheritanceToggleChange(event: MatSlideToggleChange) {
