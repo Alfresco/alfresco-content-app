5a760c0d93895a109aa26c3c6d40603c36efa832
Denys Vuika <denys.vuika@gmail.com>
2024-04-15 14:04:11 -0400

    fix name clashing with LoginComponent (#3792)

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -23,7 +23,7 @@
  */
 
 import { BlankPageComponent } from '@alfresco/adf-core';
-import { LoginComponent } from './components/login/login.component';
+import { AppLoginComponent } from './components/login/app-login.component';
 
 export const APP_ROUTES = [
   {
@@ -32,7 +32,7 @@ export const APP_ROUTES = [
   },
   {
     path: 'login',
-    component: LoginComponent,
+    component: AppLoginComponent,
     data: {
       title: 'APP.SIGN_IN'
     }
