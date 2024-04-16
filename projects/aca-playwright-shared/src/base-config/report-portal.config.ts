a12e3c99af5f1c5955936c764f26b368211907f9
akash.rathod@hyland.com <akash.rathod@hyland.com>
2024-04-16 13:21:14 +0200

    [ACS-7494] change year

--- projects/aca-playwright-shared/src/base-config/report-portal.config.ts
+++ projects/aca-playwright-shared/src/base-config/report-portal.config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2005-2023 Hyland Software, Inc. and its affiliates. All rights reserved.
+ * Copyright © 2005-2024 Hyland Software, Inc. and its affiliates. All rights reserved.
  *
  * License rights for this program may be obtained from Hyland Software, Inc.
  * pursuant to a written agreement and any use of this program without such an
