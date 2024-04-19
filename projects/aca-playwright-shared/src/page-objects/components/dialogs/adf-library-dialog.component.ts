9ee88c63f342603e53ef6248f5dde976a2d5933b
Mykyta Maliarchuk <84377976+nikita-web-ua@users.noreply.github.com>
2024-04-19 14:08:02 +0200

    [ACS-7534] folder dialog e2e fixes (#3804)

--- projects/aca-playwright-shared/src/page-objects/components/dialogs/adf-library-dialog.component.ts
+++ projects/aca-playwright-shared/src/page-objects/components/dialogs/adf-library-dialog.component.ts
@@ -37,6 +37,7 @@ export class AdfLibraryDialogComponent extends BaseComponent {
   }
 
   public getLabelText = (text: string) => this.getChild('label', { hasText: text });
+  public getRequiredMarker = (text: string) => this.getLabelText(text).locator('.mat-mdc-form-field-required-marker');
   public getDialogTitle = (text: string) => this.getChild('.mat-mdc-dialog-title', { hasText: text });
   public getErrorByText = (text: string): Locator => this.page.locator('mat-error', {hasText: text});
 
