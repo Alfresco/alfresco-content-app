04cdb55c74da8758c56b124a0b8628a8002e0f2e
akash.rathod@hyland.com <akash.rathod@hyland.com>
2024-08-16 11:35:28 +0200

    fix e2e error text

--- e2e/playwright/upload-download-actions/src/tests/upload-new-version.e2e.ts
+++ e2e/playwright/upload-download-actions/src/tests/upload-new-version.e2e.ts
@@ -78,7 +78,7 @@ test.describe('Upload new version', () => {
   async function previewUnsupportedFile(page: PersonalFilesPage, unsupportedFileName: string): Promise<void> {
     await page.dataTable.performClickFolderOrFileToOpen(unsupportedFileName);
     await page.viewer.checkUnknownFormatIsDisplayed();
-    expect(await page.viewer.getUnknownFormatMessage()).toBe(`Couldn't load preview. Unknown format.`);
+    expect(await page.viewer.getUnknownFormatMessage()).toContain(`Couldn't load preview. Unsupported file type`);
     await page.viewer.closeButtonLocator.click();
   }
 
