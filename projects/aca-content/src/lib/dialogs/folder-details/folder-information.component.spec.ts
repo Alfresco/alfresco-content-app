81a7b12aa5f46706f56545fd49c46f91eb4154fd
swapnil.verma <swapnil.verma@globallogic.com>
2024-09-16 14:39:44 +0530

    [MNT-24575] Added unit tests

--- projects/aca-content/src/lib/dialogs/folder-details/folder-information.component.spec.ts
+++ projects/aca-content/src/lib/dialogs/folder-details/folder-information.component.spec.ts
@@ -24,22 +24,45 @@
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FolderInformationComponent } from './folder-information.component';
+import { DIALOG_COMPONENT_DATA, RedirectAuthService } from '@alfresco/adf-core';
+import { ContentService } from '@alfresco/adf-content-services';
+import { By } from '@angular/platform-browser';
+import { EMPTY } from 'rxjs';
+import { LibTestingModule } from '@alfresco/aca-shared';
 
 describe('FolderInformationComponent', () => {
-  let component: FolderInformationComponent;
   let fixture: ComponentFixture<FolderInformationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FolderInformationComponent]
-    }).compileComponents();
+  const dialogData = {
+    name: 'mock-folder',
+    path: {
+      name: 'mock-folder-path'
+    },
+    createdAt: new Date(2024, 1, 1, 11, 11),
+    modifiedAt: new Date(2024, 2, 2, 22, 22)
+  };
+
+  const getValueFromElement = (id: string) => fixture.debugElement.query(By.css(`[data-automation-id="${id}"]`)).nativeElement.textContent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FolderInformationComponent, LibTestingModule],
+      providers: [
+        { provide: DIALOG_COMPONENT_DATA, useValue: dialogData },
+        { provide: RedirectAuthService, useValue: { onLogin: EMPTY, onTokenReceived: EMPTY } }
+      ]
+    });
 
     fixture = TestBed.createComponent(FolderInformationComponent);
-    component = fixture.componentInstance;
+    spyOn(TestBed.inject(ContentService), 'getNodeIcon').and.returnValue('./assets/images/ft_ic_folder.svg');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render all information in init', () => {
+    expect(getValueFromElement('folder-info-name')).toBe('mock-folder');
+    expect(getValueFromElement('folder-info-size')).toBe('APP.FOLDER_INFO.CALCULATING');
+    expect(getValueFromElement('folder-info-location')).toBe('mock-folder-path');
+    expect(getValueFromElement('folder-info-creation-date')).toBe('01/02/2024 11:11');
+    expect(getValueFromElement('folder-info-modify-date')).toBe('02/03/2024 22:22');
   });
 });
