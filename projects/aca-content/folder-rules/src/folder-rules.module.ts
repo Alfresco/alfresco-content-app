725a72cd1528e0f6835a9112cf3ef25e5a904518
swapnil.verma <swapnil.verma@globallogic.com>
2024-09-19 14:56:02 +0530

    [ACS-8694] Added rules.canManageFolderRules

--- projects/aca-content/folder-rules/src/folder-rules.module.ts
+++ projects/aca-content/folder-rules/src/folder-rules.module.ts
@@ -77,6 +77,7 @@ export class AcaFolderRulesModule {
     translation.addTranslationFolder('folder-rules', 'assets/folder-rules');
 
     extensions.setEvaluators({
+      'rules.canManageFolderRules': rules.canManageFolderRules,
       'rules.isFolderRulesEnabled': rules.isFolderRulesEnabled
     });
   }
