c4ef63065d241c3154214d9628bf66193f1ada12
DominikIwanek <dominik.iwanek@hyland.com>
2024-12-09 08:09:43 +0100

    [ACS-9068] [ACA] - Pipeline changes enabling packagage release

--- projects/aca-shared/src/lib/components/page-layout/page-layout-header.component.ts
+++ projects/aca-shared/src/lib/components/page-layout/page-layout-header.component.ts
@@ -22,13 +22,13 @@
  * from Hyland Software. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 // @deprecated Use `.aca-page-layout-header` CSS selector instead
 @Component({
   standalone: true,
   selector: 'aca-page-layout-header',
-  template: '<ng-content></ng-content>',
+  template: '<ng-content />',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'aca-page-layout-header' }
