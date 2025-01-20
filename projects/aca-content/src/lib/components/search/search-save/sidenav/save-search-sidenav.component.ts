2d68b240a6467434a8b994b5f04babf66a5522bf
MichalKinas <michal.kinas@hyland.com>
2025-01-20 09:06:25 +0100

    [ACS-9166] Remove unused components

--- projects/aca-content/src/lib/components/search/search-save/sidenav/save-search-sidenav.component.ts
+++ projects/aca-content/src/lib/components/search/search-save/sidenav/save-search-sidenav.component.ts
@@ -25,16 +25,15 @@
 import { Component, DestroyRef, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { SavedSearch, SavedSearchesService } from '@alfresco/adf-content-services';
 import { CoreModule, TranslationService } from '@alfresco/adf-core';
-import { DynamicExtensionComponent, NavBarLinkRef } from '@alfresco/adf-extensions';
+import { NavBarLinkRef } from '@alfresco/adf-extensions';
 import { ExpandMenuComponent } from '../../../sidenav/components/expand-menu.component';
-import { SidenavHeaderComponent } from '../../../sidenav/components/sidenav-header.component';
 import { AppService } from '@alfresco/aca-shared';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'aca-save-search-sidenav',
   standalone: true,
-  imports: [CoreModule, DynamicExtensionComponent, ExpandMenuComponent, SidenavHeaderComponent],
+  imports: [CoreModule, ExpandMenuComponent],
   templateUrl: './save-search-sidenav.component.html',
   encapsulation: ViewEncapsulation.None
 })
