a966a7a3e32e8a0e91b6d3794f915b7dc40d0a08
DominikIwanek <dominik.iwanek@hyland.com>
2025-03-17 08:35:12 +0100

    Angular 18 upgrade

--- projects/aca-content/src/lib/components/home/home.component.spec.ts
+++ projects/aca-content/src/lib/components/home/home.component.spec.ts
@@ -25,7 +25,7 @@
 import { HomeComponent } from './home.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppSettingsService } from '@alfresco/aca-shared';
 
@@ -36,8 +36,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HomeComponent],
-      providers: [provideHttpClient(withInterceptorsFromDi())]
+      imports: [HttpClientModule, RouterTestingModule, HomeComponent]
     });
     appSettings = TestBed.inject(AppSettingsService);
     spyOnProperty(appSettings, 'landingPage', 'get').and.returnValue('/my-mock-landing-page');
