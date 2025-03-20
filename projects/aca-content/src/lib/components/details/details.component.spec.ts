cb3a582e10ae28ebc6dc10ff101bc4fcac74e5a8
DominikIwanek <dominik.iwanek@hyland.com>
2025-03-20 09:59:05 +0100

    Fix details unit tests

--- projects/aca-content/src/lib/components/details/details.component.spec.ts
+++ projects/aca-content/src/lib/components/details/details.component.spec.ts
@@ -130,7 +130,6 @@ describe('DetailsComponent', () => {
         icon: 'highlight_off'
       } as ContentActionRef
     ];
-
     mockAspectActionsSubject$.next(extensionMock);
 
     fixture.detectChanges();
