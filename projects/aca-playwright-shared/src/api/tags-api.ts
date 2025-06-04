51345e186f84eface047cda383adffbc0e210928
DominikIwanek <dominik.iwanek@hyland.com>
2025-06-04 08:25:55 +0200

    Update unit tests

--- projects/aca-playwright-shared/src/api/tags-api.ts
+++ projects/aca-playwright-shared/src/api/tags-api.ts
@@ -40,7 +40,7 @@ export class TagsApi {
 
   async createTags(tags: TagBody[]): Promise<TagEntry | TagPaging> {
     try {
-      return this.apiService.tagsApi.createTags(tags);
+      return this.apiService.tagsApi.createTags(tags).then((entry) => entry[0]);
     } catch (error) {
       console.error(error);
       return null;
