078925261afa8755e0e5c7cd54f17ed2b8dcb41d
Mykyta Maliarchuk <84377976+nikita-web-ua@users.noreply.github.com>
2025-07-31 14:27:11 +0200

    Revert "[ACS-9374][ACS-9590] Prevent search popup flicker on Enter key press …" (#4714)
    This reverts commit c4938247782ac87803f5398ca336a9af9c660479.

--- projects/aca-content/src/lib/components/search/search-input/search-input.component.ts
+++ projects/aca-content/src/lib/components/search/search-input/search-input.component.ts
@@ -73,8 +73,6 @@ export class SearchInputComponent implements OnInit, OnDestroy {
   has400LibraryError = false;
   hasLibrariesConstraint = false;
   searchOnChange: boolean;
-  isTrimmedWordEmpty = false;
-  error = '';
 
   searchedWord: string = null;
   searchOptions: Array<SearchOptionModel> = [
@@ -153,8 +151,8 @@ export class SearchInputComponent implements OnInit, OnDestroy {
   showInputValue() {
     this.appService.setAppNavbarMode('collapsed');
     this.has400LibraryError = false;
-    this.searchedWord = this.getUrlSearchTerm();
     this.hasLibrariesConstraint = this.evaluateLibrariesConstraint();
+    this.searchedWord = this.getUrlSearchTerm();
 
     if (this.searchInputControl) {
       this.searchInputControl.searchTerm = this.searchedWord;
@@ -179,15 +177,10 @@ export class SearchInputComponent implements OnInit, OnDestroy {
    */
   onSearchSubmit(event: any) {
     const searchTerm = event.target ? (event.target as HTMLInputElement).value : event;
-    const trimmedTerm = searchTerm.trim();
+    if (searchTerm) {
+      this.searchedWord = searchTerm;
 
-    if (trimmedTerm) {
-      this.searchedWord = trimmedTerm;
-      if (this.isLibrariesChecked() && this.searchInputControl.isTermTooShort()) {
-        return;
-      } else {
-        this.searchByOption();
-      }
+      this.searchByOption();
     } else {
       this.notificationService.showError('APP.BROWSE.SEARCH.EMPTY_SEARCH');
     }
@@ -203,26 +196,15 @@ export class SearchInputComponent implements OnInit, OnDestroy {
     }
 
     this.has400LibraryError = false;
-    this.searchedWord = searchTerm;
     this.hasLibrariesConstraint = this.evaluateLibrariesConstraint();
+    this.searchedWord = searchTerm;
   }
 
   searchByOption() {
     this.syncInputValues();
     this.has400LibraryError = false;
-
-    this.searchInputControl.emitValidationError();
-
-    if (!this.searchedWord.trim()) {
-      return;
-    }
-
     if (this.isLibrariesChecked()) {
       this.hasLibrariesConstraint = this.evaluateLibrariesConstraint();
-
-      if (this.hasLibrariesConstraint) {
-        return;
-      }
       if (this.onLibrariesSearchResults && this.isSameSearchTerm()) {
         this.queryLibrariesBuilder.update();
       } else if (this.searchedWord) {
