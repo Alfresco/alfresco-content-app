564683675d45aefb74c5971129c933e4ef7f7ad3
DominikIwanek <dominik.iwanek@hyland.com>
2025-09-15 11:12:05 +0200

    [ACS-10100][Security] Remove npx usage from CI/CD and shell scripts to mitigate supply chain risks

--- scripts/generate-reports.sh
+++ scripts/generate-reports.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-./node_modules/.bin/adf-cli audit -d $DIR/docs/audit
-./node_modules/.bin/adf-cli licenses -d $DIR/docs/licences
-./node_modules/.bin/adf-cli changelog -o $DIR/docs/changelog --exclude="bot\|Alfresco Build User\|alfresco-build"
+npm run ci:audit
+npm run ci:licenses
+npm run ci:changelog
