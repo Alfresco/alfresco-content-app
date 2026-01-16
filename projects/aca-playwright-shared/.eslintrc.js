d63b6b6f985f6cdfdbca1e3a19193d4261ae278b
Adam Świderski <adam.tomasz.swiderski@gmail.com>
2026-01-16 15:43:04 +0100

    [ACS-10956] added eslint for aca-playwright-shared file

--- projects/aca-playwright-shared/.eslintrc.js
+++ projects/aca-playwright-shared/.eslintrc.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  extends: '../../.eslintrc.json',
+  extends: ['../../.eslintrc.json', '../../.eslintrc.e2e.json'],
   ignorePatterns: ['!**/*'],
   overrides: [
     {
