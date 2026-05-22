2131483eeb6c4f264d2b399ac7a4033396f11050
akashrathod28 <akash.rathod@hyland.com>
2026-05-22 15:40:07 +0200

    exclude test

--- projects/aca-playwright-shared/src/page-objects/components/viewer.component.ts
+++ projects/aca-playwright-shared/src/page-objects/components/viewer.component.ts
@@ -37,7 +37,7 @@ export class ViewerComponent extends BaseComponent {
   public shareButton = this.getChild('button[id="share-action-button"]');
   public downloadButton = this.getChild('button[id="app.viewer.download"]');
   public unknownFormat = this.getChild(`adf-viewer-unknown-format .adf-viewer__unknown-format-view`);
-  public viewerImage = this.viewerLocator.locator('.cropper-canvas img');
+  public viewerImage = this.viewerLocator.locator('#viewer-image');
   public viewerDocument = this.viewerLocator.locator('.adf-pdf-viewer__content [role="document"]');
   public documentThumbnailButton = this.getChild('[data-automation-id="adf-thumbnails-button"]');
   public thumbnailsPages = this.getChild('[data-automation-id="adf-thumbnails-content"] adf-pdf-thumb');
@@ -82,7 +82,7 @@ export class ViewerComponent extends BaseComponent {
 
   async waitForViewerContentToRender(type: 'document' | 'image' | 'media' = 'document'): Promise<void> {
     if (type === 'image') {
-      await this.viewerImage.waitFor({ state: 'visible', timeout: timeouts.extraLarge });
+      await this.viewerImage.waitFor({ state: 'attached', timeout: timeouts.extraLarge });
       return;
     }
     if (type === 'media') {
