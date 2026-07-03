982ce0c115d31aa15d270806a36b5ec095480044
Adam Świderski <adam.tomasz.swiderski@gmail.com>
2026-07-03 13:04:54 +0200

    [MNT-25681] search e2es stabilization pt 2

--- projects/aca-playwright-shared/src/page-objects/pages/search.page.ts
+++ projects/aca-playwright-shared/src/page-objects/pages/search.page.ts
@@ -77,15 +77,16 @@ export class SearchPage extends BasePage {
   public folderInformationDialog = new FolderInformationDialogComponent(this.page);
   public searchMenuCard = new SearchMenuCard(this.page);
 
-  async searchWithin(searchText: string, searchType?: SearchType, searchMode: SearchMode = 'formula'): Promise<void> {
+  async searchWithin(searchText: string, searchType?: SearchType, searchMode?: SearchMode): Promise<void> {
     if (!(await this.searchInputComponent.searchInput.isVisible())) {
       await this.acaHeader.searchButton.click();
     }
     await this.searchInputComponent.searchFor(searchText);
-    if (searchMode === 'regular') {
-      await this.searchInputComponent.regularSearchButton.click();
-    } else {
+    await this.dataTable.spinnerWaitForReload();
+    if (searchMode === 'formula') {
       await this.searchInputComponent.formulaSearchButton.click();
+    } else {
+      await this.searchInputComponent.regularSearchButton.click();
     }
     await this.searchInputComponent.searchInButton.click();
     switch (searchType) {
