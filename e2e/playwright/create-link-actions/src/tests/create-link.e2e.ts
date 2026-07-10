99b039a1c36076a9b057450be4e95780c884593e
akashrathod28 <akash.rathod@hyland.com>
2026-07-10 09:31:00 +0200

    [ACS-11871] e2e test for link document

---
+++ e2e/playwright/create-link-actions/src/tests/create-link.e2e.ts
@@ -0,0 +1,136 @@
+/*!
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Alfresco Example Content Application
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail. Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * from Hyland Software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { ApiClientFactory, NodesApi, TrashcanApi, Utils, test, LoginPage, PersonalFilesPage } from '@alfresco/aca-playwright-shared';
+import { expect } from '@playwright/test';
+
+test.describe('Create Link - creation scenarios', () => {
+  let nodesApi: NodesApi;
+  let trashcanApi: TrashcanApi;
+  const username = `user-${Utils.random()}`;
+
+  let sourceFile: string;
+  let sourceFolder: string;
+  let destinationFolder: string;
+
+  let sourceFileId: string;
+  let destinationFolderId: string;
+
+  test.beforeAll(async () => {
+    try {
+      const apiClientFactory = new ApiClientFactory();
+      await apiClientFactory.setUpAcaBackend('admin');
+      await apiClientFactory.createUser({ username });
+      nodesApi = await NodesApi.initialize(username, username);
+      trashcanApi = await TrashcanApi.initialize(username, username);
+    } catch (error) {
+      console.error(`beforeAll failed: ${error}`);
+      throw error;
+    }
+  });
+
+  test.beforeEach(async ({ personalFiles, page }) => {
+    sourceFile = `source-file-${Utils.random()}.txt`;
+    sourceFolder = `source-folder-${Utils.random()}`;
+    destinationFolder = `destination-folder-${Utils.random()}`;
+
+    const loginPage = new LoginPage(page);
+    await Utils.tryLoginUser(loginPage, username, username, 'beforeEach failed');
+
+    sourceFileId = (await nodesApi.createFile(sourceFile)).entry.id;
+    await nodesApi.createFolder(sourceFolder);
+    destinationFolderId = (await nodesApi.createFolder(destinationFolder)).entry.id;
+
+    await personalFiles.navigate();
+  });
+
+  test.afterAll(async () => {
+    await Utils.deleteNodesSitesEmptyTrashcan(nodesApi, trashcanApi, 'afterAll failed');
+  });
+
+  test('[TC1] should create a link for a file and verify it in the destination', async ({ personalFiles }) => {
+    await Utils.reloadPageIfRowNotVisible(personalFiles, sourceFile);
+    await createLinkInPersonalFiles(personalFiles, [sourceFile], destinationFolder);
+
+    const msg = await personalFiles.snackBar.getSnackBarMessage();
+    expect.soft(msg).toContain('Link created for 1 item');
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${destinationFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+
+    const expectedLinkName = `Link to ${sourceFile}.url`;
+    expect(await personalFiles.dataTable.isItemPresent(expectedLinkName)).toBe(true);
+  });
+
+  test('[TC2] should create a link for a folder and verify it in the destination', async ({ personalFiles }) => {
+    await Utils.reloadPageIfRowNotVisible(personalFiles, sourceFolder);
+    await createLinkInPersonalFiles(personalFiles, [sourceFolder], destinationFolder);
+
+    const msg = await personalFiles.snackBar.getSnackBarMessage();
+    expect.soft(msg).toContain('Link created for 1 item');
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${destinationFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+
+    const expectedLinkName = `Link to ${sourceFolder}.url`;
+    expect(await personalFiles.dataTable.isItemPresent(expectedLinkName)).toBe(true);
+  });
+
+  test('[TC3] should create links for multiple files and folders simultaneously', async ({ personalFiles }) => {
+    await Utils.reloadPageIfRowNotVisible(personalFiles, sourceFolder);
+    await createLinkInPersonalFiles(personalFiles, [sourceFile, sourceFolder], destinationFolder);
+
+    const msg = await personalFiles.snackBar.getSnackBarMessage();
+    expect.soft(msg).toContain('Links created for 2 items');
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${destinationFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+
+    expect.soft(await personalFiles.dataTable.isItemPresent(`Link to ${sourceFile}.url`)).toBe(true);
+    expect(await personalFiles.dataTable.isItemPresent(`Link to ${sourceFolder}.url`)).toBe(true);
+  });
+
+  test('[TC11] should not allow creating a duplicate link in the same destination', async ({ personalFiles }) => {
+    await nodesApi.createFileLink(sourceFileId, destinationFolderId);
+    await Utils.reloadPageIfRowNotVisible(personalFiles, sourceFile);
+
+    await personalFiles.dataTable.selectItems(sourceFile);
+    await personalFiles.clickMoreActionsButton('Create Link');
+    await personalFiles.contentNodeSelector.selectDestination(destinationFolder);
+    await personalFiles.contentNodeSelector.actionButton.click();
+
+    const msg = await personalFiles.snackBar.getSnackBarMessage();
+    expect(msg).toContain("Can't create a link, a link with the same name already exists in this location.");
+  });
+
+  const createLinkInPersonalFiles = async (personalFilesPage: PersonalFilesPage, itemNames: string[], destinationName: string): Promise<void> => {
+    for (const name of itemNames) {
+      await personalFilesPage.dataTable.selectItems(name);
+    }
+    await personalFilesPage.clickMoreActionsButton('Create Link');
+    await personalFilesPage.contentNodeSelector.selectDestination(destinationName);
+    await personalFilesPage.contentNodeSelector.actionButton.click();
+    await personalFilesPage.spinnerWaitForReload();
+  };
+});
---
+++ e2e/playwright/create-link-actions/src/tests/link-permissions.e2e.ts
@@ -0,0 +1,261 @@
+/*!
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Alfresco Example Content Application
+ *
+ * This file is part of the Alfresco Example Content Application.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail. Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * The Alfresco Example Content Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Alfresco Example Content Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * from Hyland Software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { ApiClientFactory, NodesApi, SitesApi, TrashcanApi, Utils, test, LoginPage, PersonalFilesPage } from '@alfresco/aca-playwright-shared';
+import { Site } from '@alfresco/js-api';
+import { expect } from '@playwright/test';
+
+test.describe('Create Link - comments and permissions', () => {
+  let ownerNodesApi: NodesApi;
+  let consumerNodesApi: NodesApi;
+  let ownerTrashcanApi: TrashcanApi;
+  let sitesApi: SitesApi;
+
+  const random = Utils.random();
+  const ownerUsername = `owner-${random}`;
+  const consumerUsername = `consumer-${random}`;
+
+  let siteId: string;
+  let siteTitle: string;
+  let docLibId: string;
+  let sourceFile: string;
+  let sourceFolder: string;
+  let destinationFolder: string;
+
+  let sourceFileId: string;
+  let destinationFolderId: string;
+
+  let fileLinkName: string;
+  let folderLinkName: string;
+
+  test.beforeAll(async () => {
+    try {
+      const apiClientFactory = new ApiClientFactory();
+      await apiClientFactory.setUpAcaBackend('admin');
+      await apiClientFactory.createUser({ username: ownerUsername });
+      await apiClientFactory.createUser({ username: consumerUsername });
+
+      ownerNodesApi = await NodesApi.initialize(ownerUsername, ownerUsername);
+      consumerNodesApi = await NodesApi.initialize(consumerUsername, consumerUsername);
+      ownerTrashcanApi = await TrashcanApi.initialize(ownerUsername, ownerUsername);
+      sitesApi = await SitesApi.initialize(ownerUsername, ownerUsername);
+
+      const site = await sitesApi.createSite(`link-perm-site-${random}`, Site.VisibilityEnum.PRIVATE);
+      siteId = site.entry.id;
+      siteTitle = site.entry.title;
+      docLibId = await sitesApi.getDocLibId(siteId);
+      await sitesApi.addSiteMember(siteId, consumerUsername, 'SiteConsumer');
+    } catch (error) {
+      console.error(`beforeAll failed: ${error}`);
+      throw error;
+    }
+  });
+
+  test.beforeEach(async ({ personalFiles, page }) => {
+    try {
+      sourceFile = `source-file-${Utils.random()}.txt`;
+      sourceFolder = `source-folder-${Utils.random()}`;
+      destinationFolder = `destination-folder-${Utils.random()}`;
+
+      const loginPage = new LoginPage(page);
+      await Utils.tryLoginUser(loginPage, ownerUsername, ownerUsername, 'beforeEach failed');
+
+      sourceFileId = (await ownerNodesApi.createFile(sourceFile)).entry.id;
+      const sourceFolderEntry = await ownerNodesApi.createFolder(sourceFolder);
+      destinationFolderId = (await ownerNodesApi.createFolder(destinationFolder)).entry.id;
+
+      const fileLinkEntry = await ownerNodesApi.createFileLink(sourceFileId, destinationFolderId);
+      const folderLinkEntry = await ownerNodesApi.createFolderLink(sourceFolderEntry.entry.id, destinationFolderId);
+
+      fileLinkName = fileLinkEntry.entry.name;
+      folderLinkName = folderLinkEntry.entry.name;
+
+      await personalFiles.navigate();
+    } catch (error) {
+      console.error(`beforeEach failed: ${error}`);
+      throw error;
+    }
+  });
+
+  test.afterAll(async () => {
+    await Utils.deleteNodesSitesEmptyTrashcan(ownerNodesApi, ownerTrashcanApi, 'afterAll failed', sitesApi, [siteId]);
+  });
+
+  test('[TC5a] should show a comment added on a linked file in the original file info drawer', async ({ personalFiles }) => {
+    const commentText = `comment-${Utils.random()}`;
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${destinationFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(personalFiles, fileLinkName);
+
+    await personalFiles.dataTable.getRowByName(fileLinkName).click();
+    await personalFiles.acaHeader.viewDetails.click();
+    await personalFiles.infoDrawer.commentsTab.click();
+    await expect(personalFiles.infoDrawer.commentInputField).toBeVisible();
+    await personalFiles.infoDrawer.addCommentToNode(commentText);
+
+    await personalFiles.infoDrawer.waitForComments();
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(personalFiles, sourceFile);
+
+    await personalFiles.dataTable.getRowByName(sourceFile).click();
+    await personalFiles.acaHeader.viewDetails.click();
+    await personalFiles.infoDrawer.commentsTab.click();
+    await personalFiles.infoDrawer.waitForComments();
+
+    const commentsCount = await personalFiles.infoDrawer.getCommentsCountFromList();
+    expect.soft(commentsCount).toBeGreaterThanOrEqual(1);
+
+    const commentContent = await personalFiles.infoDrawer.commentTextContent.first().innerText();
+    expect(commentContent).toContain(commentText);
+  });
+
+  test('[TC5b] should show a comment added on a linked folder in the original folder info drawer', async ({ personalFiles }) => {
+    const commentText = `comment-${Utils.random()}`;
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${destinationFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(personalFiles, folderLinkName);
+
+    await personalFiles.dataTable.getRowByName(folderLinkName).click();
+    await personalFiles.acaHeader.viewDetails.click();
+    await personalFiles.infoDrawer.commentsTab.click();
+    await expect(personalFiles.infoDrawer.commentInputField).toBeVisible();
+    await personalFiles.infoDrawer.addCommentToNode(commentText);
+
+    await personalFiles.infoDrawer.waitForComments();
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(personalFiles, sourceFolder);
+
+    await personalFiles.dataTable.getRowByName(sourceFolder).click();
+    await personalFiles.acaHeader.viewDetails.click();
+    await personalFiles.infoDrawer.commentsTab.click();
+    await personalFiles.infoDrawer.waitForComments();
+
+    const commentsCount = await personalFiles.infoDrawer.getCommentsCountFromList();
+    expect.soft(commentsCount).toBeGreaterThanOrEqual(1);
+
+    const commentContent = await personalFiles.infoDrawer.commentTextContent.first().innerText();
+    expect(commentContent).toContain(commentText);
+  });
+
+  test('[TC10a] should show Create Link action for manager and successfully create a link from a site document library', async ({
+    personalFiles
+  }) => {
+    const siteFile = `site-file-${Utils.random()}.txt`;
+    const managerDestFolder = `manager-dest-${Utils.random()}`;
+
+    await ownerNodesApi.createFile(siteFile, docLibId);
+    const managerDestFolderEntry = await ownerNodesApi.createFolder(managerDestFolder);
+    const managerDestFolderId = managerDestFolderEntry.entry.id;
+
+    await personalFiles.navigate({ remoteUrl: `./#/libraries/${docLibId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(personalFiles, siteFile);
+
+    await personalFiles.dataTable.rightClickOnItem(siteFile);
+    const createLinkButton = personalFiles.dataTable.contextMenuActions.getButtonByText('Create Link');
+    expect.soft(await createLinkButton.isVisible()).toBe(true);
+    await createLinkButton.click();
+
+    await personalFiles.contentNodeSelector.searchAndSelectDestination(managerDestFolder);
+    await personalFiles.contentNodeSelector.actionButton.click();
+
+    const msg = await personalFiles.snackBar.getSnackBarMessage();
+    expect.soft(msg).toContain('Link created for 1 item');
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${managerDestFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+
+    const expectedLinkName = `Link to ${siteFile}.url`;
+    expect(await personalFiles.dataTable.isItemPresent(expectedLinkName)).toBe(true);
+  });
+
+  test('[TC10b] should allow a consumer to create a link to a site file in their own personal files', async ({ personalFiles, page }) => {
+    const siteFile = `site-file-consumer-${Utils.random()}.txt`;
+    const consumerDestFolder = `consumer-dest-${Utils.random()}`;
+
+    await ownerNodesApi.createFile(siteFile, docLibId);
+    const consumerDestFolderEntry = await consumerNodesApi.createFolder(consumerDestFolder);
+    const consumerDestFolderId = consumerDestFolderEntry.entry.id;
+
+    const loginPage = new LoginPage(page);
+    await loginPage.logoutUser();
+    await loginPage.loginUser({ username: consumerUsername, password: consumerUsername }, { withNavigation: true, waitForLoading: true });
+
+    await personalFiles.navigate({ remoteUrl: `./#/libraries/${docLibId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(personalFiles, siteFile);
+
+    await personalFiles.dataTable.rightClickOnItem(siteFile);
+    const createLinkButton = personalFiles.dataTable.contextMenuActions.getButtonByText('Create Link');
+    expect.soft(await createLinkButton.isVisible()).toBe(true);
+    await createLinkButton.click();
+
+    await personalFiles.contentNodeSelector.searchAndSelectDestination(consumerDestFolder);
+    await personalFiles.contentNodeSelector.actionButton.click();
+
+    const msg = await personalFiles.snackBar.getSnackBarMessage();
+    expect.soft(msg).toContain('Link created for 1 item');
+
+    await personalFiles.navigate({ remoteUrl: `./#/personal-files/${consumerDestFolderId}`, waitUntil: 'load' });
+    await personalFiles.spinnerWaitForReload();
+
+    const expectedLinkName = `Link to ${siteFile}.url`;
+    expect(await personalFiles.dataTable.isItemPresent(expectedLinkName)).toBe(true);
+  });
+
+  test('[TC12] should disable the Choose button in the node selector when consumer selects a read-only library folder', async ({ page }) => {
+    const siteFile = `site-file-ro-${Utils.random()}.txt`;
+    await ownerNodesApi.createFile(siteFile, docLibId);
+
+    const loginPage = new LoginPage(page);
+    await loginPage.logoutUser();
+    await loginPage.loginUser({ username: consumerUsername, password: consumerUsername }, { withNavigation: true, waitForLoading: true });
+
+    const consumerPage = new PersonalFilesPage(page);
+
+    await consumerPage.navigate({ remoteUrl: `./#/libraries/${docLibId}`, waitUntil: 'load' });
+    await consumerPage.spinnerWaitForReload();
+    await Utils.reloadPageIfRowNotVisible(consumerPage, siteFile);
+
+    await consumerPage.dataTable.selectItems(siteFile);
+    await consumerPage.acaHeader.clickMoreActions();
+    const createLinkMenuItem = consumerPage.matMenu.getButtonByText('Create Link');
+    expect.soft(await createLinkMenuItem.isVisible()).toBe(true);
+    await createLinkMenuItem.click();
+
+    await consumerPage.contentNodeSelector.selectLocation('My Libraries');
+    await consumerPage.contentNodeSelector.spinnerWaitForReload();
+
+    await consumerPage.contentNodeSelector.selectDestination(siteTitle);
+    expect(await consumerPage.contentNodeSelector.actionButton.isDisabled()).toBe(true);
+
+    await consumerPage.contentNodeSelector.cancelButton.click();
+  });
+});
